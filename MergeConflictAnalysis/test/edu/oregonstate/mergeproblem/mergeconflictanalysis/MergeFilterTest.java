2bcc1d58223559670227dbd503abcf81c6b9e89c
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2015-01-23 13:49:08 -0800

    Detecting merge commits

--- MergeConflictAnalysis/test/edu/oregonstate/mergeproblem/mergeconflictanalysis/MergeFilterTest.java
+++ MergeConflictAnalysis/test/edu/oregonstate/mergeproblem/mergeconflictanalysis/MergeFilterTest.java
@@ -1,8 +1,12 @@
 package edu.oregonstate.mergeproblem.mergeconflictanalysis;
 
 import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.MergeResult;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.revwalk.RevWalk;
+import org.gitective.core.CommitUtils;
 import org.gitective.tests.GitTestCase;
 import org.junit.Before;
 import org.junit.Test;
@@ -10,11 +14,13 @@ import org.junit.Test;
 public class MergeFilterTest extends GitTestCase{
 	
 	private RevWalk revWalk;
+	private Repository repository;
 	private MergeFilter mergeFilter;
 
 	@Before
 	public void before() throws Exception {
-		revWalk = new RevWalk(Git.open(testRepo).getRepository());
+		repository = Git.open(testRepo).getRepository();
+		revWalk = new RevWalk(repository);
 		mergeFilter = new MergeFilter();
 	}
 
@@ -24,6 +30,19 @@ public class MergeFilterTest extends GitTestCase{
 		
 		
 		assertFalse(mergeFilter.include(revWalk, commitOne));
+	}
+	
+	@Test
+	public void testIncludeMergeCommits() throws Exception {
+		add("A", "some content");
+		branch("branch");
+		add("B", "some other content");
+		checkout("master");
+		add("A", "a change! ");
+		MergeResult mergeResult = merge("branch");
+		ObjectId newHead = mergeResult.getNewHead();
+		RevCommit mergeCommit = CommitUtils.getCommit(repository, newHead);
+		assertTrue(mergeFilter.include(revWalk, mergeCommit));
 	}
 	
 }
