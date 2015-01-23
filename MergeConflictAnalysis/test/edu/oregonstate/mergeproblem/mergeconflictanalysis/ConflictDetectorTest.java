d828d86f64e6c0eb23288e438522d7d26a0d83a2
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2015-01-23 14:38:00 -0800

    git is now a field

--- MergeConflictAnalysis/test/edu/oregonstate/mergeproblem/mergeconflictanalysis/ConflictDetectorTest.java
+++ MergeConflictAnalysis/test/edu/oregonstate/mergeproblem/mergeconflictanalysis/ConflictDetectorTest.java
@@ -12,10 +12,12 @@ import org.junit.Test;
 public class ConflictDetectorTest extends GitTestCase{
 	
 	private ConflictDetector conflictDetector;
+	private Git git;
 
 	@Before
 	public void before() throws Exception {
 		conflictDetector = new ConflictDetector();
+		git = Git.open(testRepo);
 	}
 	
 	@Test
@@ -35,6 +37,6 @@ public class ConflictDetectorTest extends GitTestCase{
 		RevCommit mergeCommit = add("A","version two+three");
 		assertEquals(2, mergeCommit.getParentCount());
 		
-		assertTrue(conflictDetector.isConflict(mergeCommit, Git.open(testRepo)));
+		assertTrue(conflictDetector.isConflict(mergeCommit, git));
 	}
 }
