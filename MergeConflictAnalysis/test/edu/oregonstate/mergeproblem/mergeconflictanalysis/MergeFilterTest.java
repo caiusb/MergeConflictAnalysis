bd629a18fa1f37ab0574b2a517632af84392797a
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2015-01-23 13:32:31 -0800

    Added the stub of a filter and a test

---
+++ MergeConflictAnalysis/test/edu/oregonstate/mergeproblem/mergeconflictanalysis/MergeFilterTest.java
@@ -0,0 +1,12 @@
+package edu.oregonstate.mergeproblem.mergeconflictanalysis;
+
+import org.gitective.tests.GitTestCase;
+import org.junit.Test;
+
+public class MergeFilterTest extends GitTestCase{
+
+	@Test
+	public void testDontIncludeRegularCommits() {
+		
+	}
+}
