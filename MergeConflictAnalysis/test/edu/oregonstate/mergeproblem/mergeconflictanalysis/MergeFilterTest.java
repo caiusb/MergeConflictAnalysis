81e0f878e58ccc6b2be3816f442a63bf182ac0de
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2015-01-23 13:41:49 -0800

    Added test to make sure I don't include regular commits

--- MergeConflictAnalysis/test/edu/oregonstate/mergeproblem/mergeconflictanalysis/MergeFilterTest.java
+++ MergeConflictAnalysis/test/edu/oregonstate/mergeproblem/mergeconflictanalysis/MergeFilterTest.java
@@ -1,12 +1,28 @@
 package edu.oregonstate.mergeproblem.mergeconflictanalysis;
 
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevWalk;
 import org.gitective.tests.GitTestCase;
+import org.junit.Before;
 import org.junit.Test;
 
 public class MergeFilterTest extends GitTestCase{
+	
+	private RevWalk revWalk;
+
+	@Before
+	public void before() throws Exception {
+		revWalk = new RevWalk(Git.open(testRepo).getRepository());
+	}
 
 	@Test
-	public void testDontIncludeRegularCommits() {
+	public void testDontIncludeRegularCommits() throws Exception {
+		RevCommit commitOne = add("A", "some content");
 		
+		MergeFilter mergeFilter = new MergeFilter();
+		
+		assertFalse(mergeFilter.include(revWalk, commitOne));
 	}
+	
 }
