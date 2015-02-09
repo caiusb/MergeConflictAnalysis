7f9a77d0d8042fc1ab40f06f4559fd138d1277fe
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2015-02-08 19:44:05 -0800

    Gives me basic statistics

--- data-processing/count-conflicts.py
+++ data-processing/count-conflicts.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/opt/local/bin/python
 
 import json
 import os
@@ -8,18 +8,46 @@ resultsFolder = sys.argv[1]
 files = os.listdir(resultsFolder)
 
 def shouldIgnore(file):
-	return not file.endswith('.json')	
+	return not file.endswith('.json')
 
-for file in files:
-	if shouldIgnore(file):
-		continue
+def convertToBool(string):
+	if (string == 'failure'):
+		return 'failure'
+	if (string == 'true'):
+		return True
+	return False
+
+def loadJson(file):
+	print(file + ':', end="")
 	pathToFile = os.path.join(resultsFolder,file)
-	print(pathToFile)
 	jsonString = ''
 	with open(pathToFile, 'r') as f:
 		for line in f:
 			if not line.startswith('Error checking out'):
 				jsonString += line
 		data = json.loads(jsonString)
+	data = {k.encode('utf8'): data[k] for k in data.keys()}
+	data = {k: {convertToBool(k1): data[k][k1] for k1 in data[k].keys()} for k in data.keys()}
+	return data
 
-	# do the dew
+def countConflicts(data):
+	conflicts = 0
+	ok = 0
+	failures = 0
+	for k in data.keys():
+		result = data[k]
+		if ('failure' in result.keys()):
+			failures = failures + 1
+		else: 
+			if (True in result.keys()):
+				conflicts = conflicts +1
+			else:
+				ok  = ok + 1
+	print(str(conflicts) + ',' + str(ok) + "," + str(failures))
+
+for file in files:
+	if shouldIgnore(file):
+		continue
+	data = loadJson(file)
+	countConflicts(data)
+
