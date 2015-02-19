cce2c432fca1962a0d9af50ae410b181532b6c78
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2015-02-19 13:26:34 -0800

    Processing the new format of files

--- data-processing/count-conflicts.py
+++ data-processing/count-conflicts.py
@@ -7,6 +7,9 @@ import sys
 resultsFolder = sys.argv[1]
 files = os.listdir(resultsFolder)
 
+filesField = 'files'
+statusField= 'status'
+
 def shouldIgnore(file):
 	return not file.endswith('.json')
 
@@ -25,24 +28,30 @@ def loadJson(file):
 		for line in f:
 			if not line.startswith('Error checking out'):
 				jsonString += line
-		data = json.loads(jsonString)
-	data = {k.encode('utf8'): data[k] for k in data.keys()}
-	data = {k: {convertToBool(k1): data[k][k1] for k1 in data[k].keys()} for k in data.keys()}
+	if (jsonString == ''):
+		jsonString = '{}'
+	data = json.loads(jsonString)
 	return data
 
 def countConflicts(data):
 	conflicts = 0
 	ok = 0
 	failures = 0
+	submodule = 0
 	for k in data.keys():
 		result = data[k]
-		if ('failure' in result.keys()):
+		#dealing with old results format
+		if (not (statusField in data[k].keys())):
+			print('0,0,0')
+			return
+		if (data[k][statusField] == 'failure'):
 			failures = failures + 1
-		else: 
-			if (True in result.keys()):
-				conflicts = conflicts +1
-			else:
-				ok  = ok + 1
+		elif (data[k][statusField] == 'conflict'):
+			conflicts = conflicts +1
+		elif (data[k][statusField] == 'clean'):
+			ok  = ok + 1
+		elif (data[k][statusField] == 'submodule'):
+			submodule = submodule + 1
 	print(str(conflicts) + ',' + str(ok) + "," + str(failures))
 
 print('Project, Conflicts, OK, Failures')
