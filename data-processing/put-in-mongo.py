d4e2bbca0c5f59ed909567cae4cc0986dd55fa40
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2015-03-18 11:06:25 -0700

    Fixed the Mongo script

--- data-processing/put-in-mongo.py
+++ data-processing/put-in-mongo.py
@@ -1,6 +1,6 @@
 #!/opt/local/bin/python
 
-import pymongo
+from pymongo import MongoClient
 import common
 import os
 import sys
@@ -10,10 +10,9 @@ resultsFolder = sys.argv[1]
 client = MongoClient()
 db = client.development
 
-def putDataInMongo(file, data) {
+def putDataInMongo(file, data) :
 	projectName = os.path.splitext(file)
-	collection = db['projectName']
-	collection.insert_many(data)
-}
+	collection = db[projectName[0]]
+	collection.insert(data)
 
 common.processJSONFilesInFolder(resultsFolder, putDataInMongo)
