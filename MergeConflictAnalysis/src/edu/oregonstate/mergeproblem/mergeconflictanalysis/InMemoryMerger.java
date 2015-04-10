004239865af56d0c6210635e8e7297024a53e3c3
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2015-04-09 17:14:41 -0700

    The merger can not merge and give me a CombinedFile

--- MergeConflictAnalysis/src/edu/oregonstate/mergeproblem/mergeconflictanalysis/InMemoryMerger.java
+++ MergeConflictAnalysis/src/edu/oregonstate/mergeproblem/mergeconflictanalysis/InMemoryMerger.java
@@ -2,6 +2,7 @@ package edu.oregonstate.mergeproblem.mergeconflictanalysis;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -19,43 +20,54 @@ public class InMemoryMerger {
 	
 	private Repository repository;
 	private List<String> names;
-
+	
 	public InMemoryMerger(Repository repository) {
 		this.repository = repository;
 		names = Arrays.asList(new String[]{"base", "A", "B"});
 	}
 
-	public void merge(RevCommit first, RevCommit second) throws IOException {
+	public Map<String, CombinedFile> merge(RevCommit first, RevCommit second) throws IOException {
+		Map<String, CombinedFile> results = new HashMap<String, CombinedFile>();
+		
 		RecursiveMerger recursiveMerger = (RecursiveMerger) new StrategyRecursive().newMerger(repository, true);
 		recursiveMerger.merge(first, second);
 		
 		Map<String, MergeResult<? extends Sequence>> mergeResults = recursiveMerger.getMergeResults();
 		for (String touchedFile : mergeResults.keySet()) {
 			MergeResult<? extends Sequence> mergeResult = mergeResults.get(touchedFile);
-			processMergeResult(mergeResult);
+			CombinedFile fileResult = processMergeResult(mergeResult);
+			results.put(touchedFile, fileResult);
 		}
+		
+		return results;
 	}
 
-	private void processMergeResult(MergeResult<? extends Sequence> mergeResult) {
+	private CombinedFile processMergeResult(MergeResult<? extends Sequence> mergeResult) {
 		List<? extends Sequence> sequences = mergeResult.getSequences();
-		mergeResult.forEach((chunk) -> processChunk(chunk, sequences));
+		CombinedFile combinedFile = new CombinedFile();
+		mergeResult.forEach((chunk) -> processChunk(chunk, sequences, combinedFile));
+		
+		return combinedFile;
 	}
 
-	private void processChunk(MergeChunk mergeChunk, List<? extends Sequence> sequences) {
+	private void processChunk(MergeChunk mergeChunk, List<? extends Sequence> sequences, CombinedFile combinedFile) {
 		ConflictState conflictState = mergeChunk.getConflictState();
 		int beginIndex = mergeChunk.getBegin();
 		int endIndex = mergeChunk.getEnd();
 		int sequenceIndex = mergeChunk.getSequenceIndex();
 		
-		if (conflictState.equals(ConflictState.FIRST_CONFLICTING_RANGE))
-			System.out.println(">>>>" + names.get(sequenceIndex));
-		if (conflictState.equals(ConflictState.NEXT_CONFLICTING_RANGE))
-			System.out.println("=====" + names.get(sequenceIndex));
-		
 		RawText textSequence = (RawText) sequences.get(sequenceIndex);
 		String actualSequenceText = getTextForLines(textSequence, beginIndex, endIndex);
-
-		System.out.println(actualSequenceText);
+		
+		ChunkOwner owner = null;
+		if (conflictState.equals(ConflictState.FIRST_CONFLICTING_RANGE))
+			owner = ChunkOwner.A;
+		if (conflictState.equals(ConflictState.NEXT_CONFLICTING_RANGE))
+			owner = ChunkOwner.B;
+		if (conflictState.equals(ConflictState.NO_CONFLICT))
+			owner = ChunkOwner.BOTH;
+		
+		combinedFile.addChunk(owner, actualSequenceText);
 	}
 
 	private String getTextForLines(RawText textSequence, int beginIndex, int endIndex) {
--- MergeConflictAnalysis/test/edu/oregonstate/mergeproblem/mergeconflictanalysis/InMemoryMergerTest.java
+++ MergeConflictAnalysis/test/edu/oregonstate/mergeproblem/mergeconflictanalysis/InMemoryMergerTest.java
@@ -1,5 +1,7 @@
 package edu.oregonstate.mergeproblem.mergeconflictanalysis;
 
+import java.util.Map;
+
 import org.eclipse.jgit.revwalk.RevCommit;
 import org.junit.Before;
 import org.junit.Test;
@@ -14,7 +16,7 @@ public class InMemoryMergerTest extends MergeGitTest {
 	}
 	
 	@Test
-	public void testEmptyMerge() throws Exception {
+	public void testConflictingMerge() throws Exception {
 		RevCommit commit = createConflictingCommit();
 		RevCommit[] parents = commit.getParents();
 		assertEquals(2, parents.length);
@@ -22,7 +24,27 @@ public class InMemoryMergerTest extends MergeGitTest {
 		RevCommit first = parents[0];
 		RevCommit second = parents[1];
 		
-		merger.merge(first, second);
+		Map<String, CombinedFile> result = merger.merge(first, second);
+		assertEquals(1, result.keySet().size());
+		
+		CombinedFile AFile = result.get("A.java");
+		assertNotNull(AFile);
+		
+		String aVersion = AFile.getVersion(ChunkOwner.A);
+		assertEquals("public class conflictingA3{}\n", aVersion);
+		
+		String bVersion = AFile.getVersion(ChunkOwner.B);
+		assertEquals("public class A2{}\n", bVersion);
+	}
+	
+	@Test
+	public void testNonConflictingMerge() throws Exception {
+		RevCommit merge = createNonConflictingMerge();
+		RevCommit[] parents = merge.getParents();
+		assertEquals(2, parents.length);
+		
+		Map<String, CombinedFile> result = merger.merge(parents[0], parents[1]);
+		assertEquals(0, result.keySet().size());
 	}
 
 }
