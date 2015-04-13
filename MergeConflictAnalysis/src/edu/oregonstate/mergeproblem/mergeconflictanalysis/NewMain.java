81d4b70577921aadc6b2f4367d9c57bd5414891c
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2015-04-13 13:46:19 -0700

    Doing a Myers diff

--- MergeConflictAnalysis/src/edu/oregonstate/mergeproblem/mergeconflictanalysis/NewMain.java
+++ MergeConflictAnalysis/src/edu/oregonstate/mergeproblem/mergeconflictanalysis/NewMain.java
@@ -5,10 +5,16 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.diff.DiffAlgorithm;
+import org.eclipse.jgit.diff.DiffAlgorithm.SupportedAlgorithm;
+import org.eclipse.jgit.diff.RawText;
+import org.eclipse.jgit.diff.RawTextComparator;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
 
 public class NewMain {
+
+	private static DiffAlgorithm diffAlgorithm = DiffAlgorithm.getAlgorithm(SupportedAlgorithm.MYERS);;
 
 	public static void main(String[] args) throws Exception {
 		for (String repositoryPath : args) {
@@ -20,15 +26,40 @@ public class NewMain {
 			List<CommitStatus> statuses = mergeCommits.stream().parallel()
 					.map((commit) -> merger.recreateMerge(commit))
 					.collect(Collectors.toList());
+			
+			final StringBuilder stringBuilder = new StringBuilder();
+			stringBuilder.append("SHA, FILE, A_TO_B, A_TO_SOLVED, B_TO_SOLVED\n");
+			statuses.stream().parallel().forEach((status) ->{
+				List<String> listOfConflictingFiles = status.getListOfConflictingFiles();
+				for (String file : listOfConflictingFiles) {
+					if (!file.endsWith("java"))
+						continue;
+					String solvedVersion = status.getSolvedVersion(file);
+					CombinedFile combinedFile = status.getCombinedFile(file);
+					String aVersion = combinedFile.getVersion(ChunkOwner.A);
+					String bVersion = combinedFile.getVersion(ChunkOwner.B);
+					int aToB = -1;
+					int aToSolved = -1;
+					int bToSolved = -1;
+					
+					if (aVersion != null && bVersion != null && solvedVersion != null) {
+						aToB = getDiffSize(aVersion, bVersion);
+						aToSolved = getDiffSize(aVersion, solvedVersion);
+						bToSolved = getDiffSize(bVersion, solvedVersion);
+					}
+					stringBuilder.append(status.getSHA1() + "," + file + "," + aToB + "," + aToSolved + "," + bToSolved + "\n");
+				}
+			});
+						
 			long finish = System.nanoTime();
 			
-			String results = statuses.stream().parallel()
-					.map((status) -> status.toJSONString())
-					.collect(Collectors.joining(",\n"));
+			System.out.println(stringBuilder.toString());
 			
-			System.out.println("[" + results + "]");
-			
-			System.out.println("The processing took " + (finish - start)/1000000 + " miliseconds");
+//			System.out.println("The processing took " + (finish - start)/1000000 + " miliseconds");
 		}
+	}
+
+	private static int getDiffSize(String aVersion, String bVersion) {
+		return diffAlgorithm.diff(RawTextComparator.DEFAULT, new RawText(aVersion.getBytes()), new RawText(bVersion.getBytes())).size();
 	}
 }
