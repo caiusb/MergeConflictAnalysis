114223cc84351c9b436f6756d4dd8f3f262697d5
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2015-04-21 12:51:14 -0700

    A few more tests, for sanity

--- MergeConflictAnalysis/test/edu/oregonstate/mergeproblem/mergeconflictanalysis/ASTDiffTest.java
+++ MergeConflictAnalysis/test/edu/oregonstate/mergeproblem/mergeconflictanalysis/ASTDiffTest.java
@@ -2,6 +2,7 @@ package edu.oregonstate.mergeproblem.mergeconflictanalysis;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.junit.Test;
@@ -17,4 +18,24 @@ public class ASTDiffTest {
 		assertEquals(0, actions.size());
 	}
 
+	@Test
+	public void testDiffGarbage() throws IOException {
+		ASTDiff astDiff = new ASTDiff();
+		List<Action> actions = astDiff.getActions("fdkaljfka", "fjkd");
+		assertEquals(0, actions.size());
+	}
+	
+	@Test
+	public void testValidWithEmpty() throws Exception {
+		ASTDiff astDiff = new ASTDiff();
+		List<Action> actions = astDiff.getActions("public class A{}", "");
+		assertEquals(3, actions.size());
+	}
+	
+	@Test
+	public void testValid() throws Exception {
+		ASTDiff astDiff = new ASTDiff();
+		List<Action> actions = astDiff.getActions("public class A {}", "public class B {}");
+		assertEquals(6, actions.size());
+	}
 }
