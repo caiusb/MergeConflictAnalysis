be06b55c5e225310921a723372ab2bb20f6509d7
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2015-04-21 14:27:28 -0700

    Added a before method

--- MergeConflictAnalysis/test/edu/oregonstate/mergeproblem/mergeconflictanalysis/ASTDiffTest.java
+++ MergeConflictAnalysis/test/edu/oregonstate/mergeproblem/mergeconflictanalysis/ASTDiffTest.java
@@ -5,36 +5,40 @@ import static org.junit.Assert.assertEquals;
 import java.io.IOException;
 import java.util.List;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import fr.labri.gumtree.actions.model.Action;
 
 public class ASTDiffTest {
 	
+	private ASTDiff astDiff;
+	
+	@Before
+	public void before() {
+		astDiff = new ASTDiff();
+	}
+	
 	@Test
 	public void testDiffEmptyFiles() throws Exception {
-		ASTDiff astDiff = new ASTDiff();
 		List<Action> actions = astDiff.getActions("", "");
 		assertEquals(0, actions.size());
 	}
 
 	@Test
 	public void testDiffGarbage() throws IOException {
-		ASTDiff astDiff = new ASTDiff();
 		List<Action> actions = astDiff.getActions("fdkaljfka", "fjkd");
 		assertEquals(0, actions.size());
 	}
 	
 	@Test
 	public void testValidWithEmpty() throws Exception {
-		ASTDiff astDiff = new ASTDiff();
 		List<Action> actions = astDiff.getActions("public class A{}", "");
 		assertEquals(3, actions.size());
 	}
 	
 	@Test
 	public void testValid() throws Exception {
-		ASTDiff astDiff = new ASTDiff();
 		List<Action> actions = astDiff.getActions("public class A {}", "public class B {}");
 		assertEquals(6, actions.size());
 	}
