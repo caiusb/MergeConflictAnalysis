c84d2b0ac1deed9cb7b9d9384b623ceb8b44ab4f
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2015-04-22 19:57:40 -0700

    Correctly creating project and commit folders

--- MergeConflictAnalysis/src/edu/oregonstate/mergeproblem/mergeconflictanalysis/VisualizationDataGenerator.java
+++ MergeConflictAnalysis/src/edu/oregonstate/mergeproblem/mergeconflictanalysis/VisualizationDataGenerator.java
@@ -1,11 +1,18 @@
 package edu.oregonstate.mergeproblem.mergeconflictanalysis;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 public class VisualizationDataGenerator {
 	
 	public void generateData(String projectName, List<CommitStatus> statuses, String targetFolder) {
-		
+		Path projectPath = Paths.get(targetFolder).resolve(projectName);
+		projectPath.toFile().mkdir();
+		statuses.forEach((status) -> {
+			String sha1 = status.getSHA1();
+			projectPath.resolve(sha1).toFile().mkdir();
+		});
 	}
 
 }
--- MergeConflictAnalysis/test/edu/oregonstate/mergeproblem/mergeconflictanalysis/VisualizationDataGeneratorTest.java
+++ MergeConflictAnalysis/test/edu/oregonstate/mergeproblem/mergeconflictanalysis/VisualizationDataGeneratorTest.java
@@ -1,14 +1,52 @@
 package edu.oregonstate.mergeproblem.mergeconflictanalysis;
 
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 public class VisualizationDataGeneratorTest {
 	
 	private VisualizationDataGenerator generator;
+	private Path tempDirectory;
+	private String tempFolderAbsolutePath;
 
 	@Before
-	public void before() {
+	public void before() throws Exception {
 		generator = new VisualizationDataGenerator();
+		tempDirectory = Files.createTempDirectory("");
+		tempFolderAbsolutePath = tempDirectory.toFile().getAbsoluteFile().getAbsolutePath();
+	}
+	
+	@After
+	public void after() {
+		tempDirectory.toFile().delete();
+	}
+	
+	@Test
+	public void testCreateProjectFolder() {
+		generator.generateData("testProject", new ArrayList<CommitStatus>(), tempFolderAbsolutePath);
+		
+		File projectFolder = tempDirectory.resolve("testProject").toFile();
+		assertTrue(projectFolder.exists());
+		assertTrue(projectFolder.isDirectory());
+	}
+	
+	@Test
+	public void testCreateCommitFolder() {
+		CommitStatus commitStatus = new CommitStatus(null, "abcde", new HashMap<String, CombinedFile>());
+		generator.generateData("testProject", Arrays.asList(new CommitStatus[]{commitStatus}), tempFolderAbsolutePath);
+		File commitFolder = tempDirectory.resolve("testProject").resolve("abcde").toFile();
+		assertTrue(commitFolder.exists());
+		assertTrue(commitFolder.isDirectory());
 	}
 
 }
