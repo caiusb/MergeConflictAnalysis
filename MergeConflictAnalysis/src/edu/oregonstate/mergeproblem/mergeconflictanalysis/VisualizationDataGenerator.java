148ee637f36ed02596ad83ab70108d945df4cef6
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2015-04-22 19:44:17 -0700

    Stub class and it's test for generating the diffs

---
+++ MergeConflictAnalysis/src/edu/oregonstate/mergeproblem/mergeconflictanalysis/VisualizationDataGenerator.java
@@ -0,0 +1,11 @@
+package edu.oregonstate.mergeproblem.mergeconflictanalysis;
+
+import java.util.List;
+
+public class VisualizationDataGenerator {
+	
+	public void generateData(String projectName, List<CommitStatus> statuses, String targetFolder) {
+		
+	}
+
+}
---
+++ MergeConflictAnalysis/test/edu/oregonstate/mergeproblem/mergeconflictanalysis/VisualizationDataGeneratorTest.java
@@ -0,0 +1,14 @@
+package edu.oregonstate.mergeproblem.mergeconflictanalysis;
+
+import org.junit.Before;
+
+public class VisualizationDataGeneratorTest {
+	
+	private VisualizationDataGenerator generator;
+
+	@Before
+	public void before() {
+		generator = new VisualizationDataGenerator();
+	}
+
+}
