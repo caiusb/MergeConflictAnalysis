2f89dbae9820f50df5ccb909b31360289ebd97ff
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2015-04-27 09:41:37 -0700

    Added an ASTFileGenerator

---
+++ MergeConflictAnalysis/src/edu/oregonstate/mergeproblem/mergeconflictanalysis/ASTFileGenerator.java
@@ -0,0 +1,38 @@
+package edu.oregonstate.mergeproblem.mergeconflictanalysis;
+
+import fr.labri.gumtree.client.StringHtmlDiff;
+
+public class ASTFileGenerator {
+	
+	private String urlFolder;
+	
+	public ASTFileGenerator() {
+		this("");
+	}
+
+	public ASTFileGenerator(String urlFolder) {
+		this.urlFolder = urlFolder;
+	}
+	
+	public String generateIndex() {
+		return "<head>\n" + 
+				"	<meta charset=\"utf-8\" />\n" + 
+				"	<title>Merge Visualizer</title>\n" + 
+				"</head>\n" + 
+				"\n" + 
+				"<body>\n" + 
+				"	<a href=\"atob.html\">A to B</a><br />\n" + 
+				"	<a href=\"atos.html\">A to Solved</a><br />\n" + 
+				"	<a href=\"btos.html\">B to Solved</a><br />\n" + 
+				"</body>";
+	}
+	
+	public String generateDiff(String aContents, String bContents) {
+		try {
+			return new StringHtmlDiff().getHtmlOfDiff(urlFolder, aContents, bContents);		
+		} catch (Exception e) {
+			return "There was an exception generating this file: \n " + e.getMessage();
+		}
+	}
+
+}
