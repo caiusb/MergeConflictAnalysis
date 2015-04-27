265340884b1d13fdfeb137760365cfa5fb8dc14f
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2015-04-27 15:00:47 -0700

    Define the axis functions as global variables

--- d3js/data-visualizer.js
+++ d3js/data-visualizer.js
@@ -1,3 +1,6 @@
+var xAxisFunction = function(d) { return d.LOC_A_TO_SOLVED; }
+var yAxisFunction = function(d) { return d.LOC_B_TO_SOLVED; }
+
 function goToData(d) {
 	project = d.PROJECT;
 	commit = d.SHA;
