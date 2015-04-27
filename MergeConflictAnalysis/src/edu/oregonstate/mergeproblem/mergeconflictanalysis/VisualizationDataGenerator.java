8ffd76191cec8c3e3a3d1b49d441a7e163c08faf
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2015-04-26 17:00:13 -0700

    Extracted a method

--- MergeConflictAnalysis/src/edu/oregonstate/mergeproblem/mergeconflictanalysis/VisualizationDataGenerator.java
+++ MergeConflictAnalysis/src/edu/oregonstate/mergeproblem/mergeconflictanalysis/VisualizationDataGenerator.java
@@ -25,26 +25,30 @@ public class VisualizationDataGenerator {
 			status.getListOfConflictingFiles().forEach((file) -> {
 				Path filePath = commitPath.resolve(file);
 				filePath.toFile().mkdirs();
-				Path locPath = filePath.resolve("loc");
-				locPath.toFile().mkdir();
-				
-				Path a = locPath.resolve("A");
-				Path b = locPath.resolve("B");
-				Path s = locPath.resolve("S");
-				Path index=locPath.resolve("index.html");
-				try {
-					a.toFile().createNewFile();
-					Files.write(a, status.getCombinedFile(file).getVersion(ChunkOwner.A).getBytes(), StandardOpenOption.WRITE);
-					b.toFile().createNewFile();
-					Files.write(b, status.getCombinedFile(file).getVersion(ChunkOwner.B).getBytes(), StandardOpenOption.WRITE);
-					s.toFile().createNewFile();
-					Files.write(s, status.getSolvedVersion(file).getBytes(), StandardOpenOption.WRITE);
-					index.toFile().createNewFile();
-					Files.write(index, locIndexHtml.getIndex().getBytes(), StandardOpenOption.WRITE);
-				} catch (Exception e) {
-				}
+				generateLOCData(locIndexHtml, status, file, filePath);
 			}); 
 		});
+	}
+
+	private void generateLOCData(LOCIndexHtml locIndexHtml, CommitStatus status, String fileName, Path pathToPopulate) {
+		Path locPath = pathToPopulate.resolve("loc");
+		locPath.toFile().mkdir();
+		
+		Path a = locPath.resolve("A");
+		Path b = locPath.resolve("B");
+		Path s = locPath.resolve("S");
+		Path index=locPath.resolve("index.html");
+		try {
+			a.toFile().createNewFile();
+			Files.write(a, status.getCombinedFile(fileName).getVersion(ChunkOwner.A).getBytes(), StandardOpenOption.WRITE);
+			b.toFile().createNewFile();
+			Files.write(b, status.getCombinedFile(fileName).getVersion(ChunkOwner.B).getBytes(), StandardOpenOption.WRITE);
+			s.toFile().createNewFile();
+			Files.write(s, status.getSolvedVersion(fileName).getBytes(), StandardOpenOption.WRITE);
+			index.toFile().createNewFile();
+			Files.write(index, locIndexHtml.getIndex().getBytes(), StandardOpenOption.WRITE);
+		} catch (Exception e) {
+		}
 	}
 
 }
