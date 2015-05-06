d8bbe46de95fc2ba989d37c5387f1101bb3d9207
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2015-05-06 12:48:38 -0700

    Moved the javascript to it's file

--- d3js/data-visualizer.js
+++ d3js/data-visualizer.js
@@ -7,3 +7,35 @@ function goToData(d) {
 	file = d.FILE;
 	window.location.href = "data/" + project + "/" + commit + "/" +file + "/loc/";
 }
+
+function drawPlot() {
+	var myScatterPlot = scatterPlot()
+		.height(600)
+		.width(parseInt(d3.select("body").style("width")))
+		.circleRadius(4);
+
+	d3.csv("all.csv", function(error, data) {
+
+		//convert strings into numbers
+		data.forEach(function(d) {
+			d.LOC_A_TO_B = +d.LOC_A_TO_B;
+			d.LOC_A_TO_SOLVED = +d.LOC_A_TO_SOLVED;
+			d.LOC_B_TO_SOLVED = +d.LOC_B_TO_SOLVED;
+
+			d.AST_A_TO_B = +d.AST_A_TO_B;
+			d.AST_A_TO_SOLVED = +d.AST_A_TO_SOVLED;
+			d.AST_B_TO_SOLVED = +d.AST_B_TO_SOLVED;
+		});
+
+	  	//data to be shown
+	  	myScatterPlot.xValue(xAxisFunction)
+	    	.yValue(yAxisFunction)
+	    	.label(function(d) { return d.PROJECT + "\n" + d.SHA + "\n" + d.FILE; })
+	    	.category(function(d) { return d.PROJECT; })
+			.clickAction(goToData);
+
+		myScatterPlot.legends=[];
+
+		d3.select("#scatterplot").datum(data).call(myScatterPlot);
+	});
+}
