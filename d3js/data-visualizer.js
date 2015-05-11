87cf5a89a7130459133de3a6dbc2442b1d8ac181
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2015-05-11 13:55:50 -0700

    Creating the table head

--- d3js/data-visualizer.js
+++ d3js/data-visualizer.js
@@ -1,11 +1,56 @@
 var xAxisFunction = function(d) { return d.LOC_A_TO_SOLVED; }
 var yAxisFunction = function(d) { return d.LOC_B_TO_SOLVED; }
 
+var columnsToTabulate = ["PROJECT", "SHA", "FILE", "AST", "LOC"];
+
+function dataKey (d) { 
+	return d.SHA + d.PROJECT + d.FILE; 
+}
+
+function getProjectName(d) {
+	return d.PROJECT;
+}
+
+function getFile(d) {
+	return d.FILE;
+}
+
+function getSHA(d) {
+	return d.SHA;
+}
+
+function createTableHead(columns) {
+	var table = d3.select("body").append("table");
+	head = table.append("thead");
+
+	head.append("tr")
+		.selectAll("th")
+		.data(columns)
+		.enter()
+		.append("th")
+		.text(function(column) { return column; });
+}
+
+function tabulate(data, columns) {
+
+	dataRows = body.selectAll("tr").data(points, dataKey);
+	//dataRows.exit().remove();
+	rows = dataRows.enter().append("tr");
+	rows.selectAll("td");
+}
+
 function goToData(d) {
 	project = d.PROJECT;
 	commit = d.SHA;
 	file = d.FILE;
-	window.location.href = "data/" + project + "/" + commit + "/" +file + "/loc/";
+	//window.location.href = "data/" + project + "/" + commit + "/" +file + "/loc/";
+
+	points = d3.selectAll("circle").data()
+		.filter(function (data) {
+			return (xAxisFunction(data) == xAxisFunction(d) && yAxisFunction(data) == yAxisFunction(d));
+		});
+
+	tabulate(points, columnsToTabulate);
 }
 
 function drawPlot() {
@@ -32,11 +77,13 @@ function drawPlot() {
 	    	.yValue(yAxisFunction)
 	    	.label(function (d) { return d.PROJECT + "\n" + d.SHA + "\n" + d.FILE; })
 	    	.category(function (d) { return d.PROJECT; })
-	    	.dataKey(function (d) { return d.SHA + d.PROJECT + d.FILE; })
+	    	.dataKey(dataKey)
 			.clickAction(goToData);
 
 		myScatterPlot.legends=[];
 
 		d3.select("#scatterplot").datum(data).call(myScatterPlot);
+
+		createTableHead(columnsToTabulate);
 	});
 }
