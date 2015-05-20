346a8aa365786991b0baff52fc5e18d69ba1d639
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2015-05-20 13:56:23 -0700

    Added method to plot two columns of a data frame
    And do linear regressions and the like

--- R/distance-metrics.R
+++ R/distance-metrics.R
@@ -15,3 +15,13 @@ plotDistanceMetrics <<- function(data) {
   plot(data$AST_A_TO_B, data$AST_A_TO_SOLVED)
   plot(data$AST_A_TO_B, data$AST_B_TO_SOLVED)
 }
+
+plotWithLinearRegression <<- function(data, x, y) {
+  trim <- data[data[x] > 1, ]
+  trim <- trim[trim[y] > 1, ]
+  plot(trim[[x]], trim[[y]], xlab=x, ylab=y)
+  fit = lm(data[[y]] ~ data[[x]], data=trim)
+  abline(fit, col="red")
+  print(summary(fit))
+  print(cor.test(trim[[x]], trim[[y]]))
+}
