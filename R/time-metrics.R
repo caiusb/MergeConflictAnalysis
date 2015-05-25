f977529eb907ab2bcf862c44479941617d240899
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2015-05-25 15:31:20 -0700

    More work on the time metrics

--- R/time-metrics.R
+++ R/time-metrics.R
@@ -1,6 +1,16 @@
+source('common.R')
+
 plotTimeMetrics <<- function(data) {
   data$RESOLUTION_TIME <- data$TIME_SOLVED - data$TIME_B
-  data$EFFORT <- (data$LOC_A_TO_SOLVED + data$LOC_B_TO_SOLVED);
-  plot(data$EFFORT, data$RESOLUTION_TIME)
+  data$EFFORT <- calculateEffort(data)
+  print(data$EFFORT)
+  plotWithLinearRegression(data, 'EFFORT','RESOLUTION_TIME')
   return(data)
+}
+
+calculateEffort <- function(data) {
+  averageConflictSize <- (data$LOC_SIZE_A + data$LOC_SIZE_B)/2
+  deviationFromDiagonal <- sqrt((data$LOC_A_TO_SOLVED^2) + (data$LOC_B_TO_SOLVED)^2)
+  effort <- deviationFromDiagonal
+  return(effort)
 }
