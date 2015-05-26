3099ca440ad6d6b9de3dfb3b686e5e1efa9c4833
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2015-05-26 16:28:14 -0700

    Added a processor to collect merge bases

---
+++ MergeConflictAnalysis/src/edu/oregonstate/mergeproblem/mergeconflictanalysis/processors/MergeBaseProcessor.java
@@ -0,0 +1,26 @@
+package edu.oregonstate.mergeproblem.mergeconflictanalysis.processors;
+
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.gitective.core.CommitUtils;
+
+import edu.oregonstate.mergeproblem.mergeconflictanalysis.CommitStatus;
+
+public class MergeBaseProcessor implements FileProcessor {
+
+	@Override
+	public String getHeader() {
+		return "BASE_SHA1,BASE_TIME";
+	}
+
+	@Override
+	public String getDataForMerge(CommitStatus status, String fileName) {
+		String mergeSHA = status.getSHA1();
+		Repository repository = status.getRepository();
+		RevCommit mergeCommit = CommitUtils.getCommit(repository, mergeSHA);
+		RevCommit[] parents = mergeCommit.getParents();
+		RevCommit base = CommitUtils.getBase(repository, parents[0], parents[1]);
+		return base.getName() + "," + base.getCommitTime();
+	}
+
+}
---
+++ MergeConflictAnalysis/test/edu/oregonstate/mergeproblem/mergeconflictanalysis/processors/MergeBaseProcessorTest.java
@@ -0,0 +1,37 @@
+package edu.oregonstate.mergeproblem.mergeconflictanalysis.processors;
+
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.gitective.core.CommitUtils;
+import org.junit.Before;
+import org.junit.Test;
+
+import edu.oregonstate.mergeproblem.mergeconflictanalysis.CommitStatus;
+import edu.oregonstate.mergeproblem.mergeconflictanalysis.InMemoryMerger;
+import edu.oregonstate.mergeproblem.mergeconflictanalysis.MergeGitTest;
+
+public class MergeBaseProcessorTest extends MergeGitTest {
+
+	private MergeBaseProcessor processor;
+	
+	@Before
+	public void before() throws Exception {
+		super.before();
+		processor = new MergeBaseProcessor();
+	}
+
+	@Test
+	public void testHeader() {
+		String header = processor.getHeader();
+		assertEquals("BASE_SHA1,BASE_TIME", header);
+	}
+	
+	@Test
+	public void testData() throws Exception {
+		RevCommit mergeCommit = createConflictingCommit();
+		RevCommit base = CommitUtils.getCommit(repository, "HEAD~2");
+		CommitStatus commitStatus = new InMemoryMerger(repository).recreateMerge(mergeCommit);
+		String data = processor.getDataForMerge(commitStatus, commitStatus.getListOfConflictingFiles().get(0));
+		String expected = base.getName() + "," + base.getCommitTime();
+		assertEquals(expected, data);
+	}
+}
