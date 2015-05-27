c939626e8815d84fa67669a028ddae4c9bbf265c
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2015-05-27 09:04:05 -0700

    Moved the data loading to the common file

--- R/common.R
+++ R/common.R
@@ -1,3 +1,5 @@
+resultsFolder <<- "../../results"
+
 plotWithLinearRegression <<- function(data, x, y) {
   trim <- data[data[x] > 1, ]
   trim <- trim[trim[y] > 1, ]
@@ -6,4 +8,42 @@ plotWithLinearRegression <<- function(data, x, y) {
   abline(fit, col="red")
   print(summary(fit))
   print(cor.test(trim[[x]], trim[[y]]))
+}
+
+loadData <<- function(folder) {
+  
+  files <- list.files(path=resultsFolder, pattern="*.csv", full.names=T, recursive=FALSE)
+  data <- data.frame(SHA = character(0),
+                     FILE = character(0),
+                     BASE_SHA = character(0),
+                     BASE_TIME = integer(0), 
+                     TIME_A = integer(0),
+                     TIME_B = integer(0),
+                     TIME_SOLVED = integer(0),
+                     LOC_A_TO_B = integer(0),
+                     LOC_A_TO_SOLVED = integer(0),
+                     LOC_B_TO_SOLVED = integer(0),
+                     AST_A_TO_B = integer(0),
+                     AST_A_TO_SOLVED = integer(0),
+                     AST_B_TO_SOLVED = integer(0),
+                     LOC_SIZE_A = integer(0),
+                     LOC_SIZE_B = integer(0),
+                     LOC_SIZE_SOLVED = integer(0),
+                     AST_SIZE_A = integer(0),
+                     AST_SIZE_B = integer(0),
+                     AST_SIZE_SOLVED = integer(0))
+  
+  lapply(files, function(file) {
+    fileLength = length(readLines(file))
+    if (fileLength <= 1)
+      return 
+    else {
+      print(file)
+      fileInfo <- file.info(file)
+      currentDataFile <- read.csv(file, header=T, sep=',', blank.lines.skip=T, as.is=T)
+      data <<- rbind(data, currentDataFile)
+    }
+  })
+  
+  return(data)
 }
--- R/diff-stats.R
+++ R/diff-stats.R
@@ -2,38 +2,7 @@ source("common.R")
 source("distance-metrics.R")
 source("time-metrics.R")
 
-resultsFolder = "../../results"
-
-files <- list.files(path=resultsFolder, pattern="*.csv", full.names=T, recursive=FALSE)
-data <- data.frame(SHA = character(0),
-                   FILE = character(0),
-                   TIME_A = integer(0),
-                   TIME_B = integer(0),
-                   TIME_SOLVED = integer(0),
-                   LOC_A_TO_B = integer(0),
-                   LOC_A_TO_SOLVED = integer(0),
-                   LOC_B_TO_SOLVED = integer(0),
-                   AST_A_TO_B = integer(0),
-                   AST_A_TO_SOLVED = integer(0),
-                   AST_B_TO_SOLVED = integer(0),
-                   LOC_SIZE_A = integer(0),
-                   LOC_SIZE_B = integer(0),
-                   LOC_SIZE_SOLVED = integer(0),
-                   AST_SIZE_A = integer(0),
-                   AST_SIZE_B = integer(0),
-                   AST_SIZE_SOLVED = integer(0))
-
-lapply(files, function(file) {
-  fileLength = length(readLines(file))
-  if (fileLength <= 1)
-    return 
-  else {
-    print(file)
-    fileInfo <- file.info(file)
-    currentDataFile <- read.csv(file, header=T, sep=',', blank.lines.skip=T, as.is=T)
-    data <<- rbind(data, currentDataFile)
-  }
-})
+data <- loadData(resultsFolder)
 
 #Trimming the cases where I couldn't find a diff, for various reasons...
 data <- data[data$LOC_A_TO_SOLVED >= 0, ]
