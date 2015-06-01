4112b6e2e86cbd39105caac573567a5aba7c0215
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2015-06-01 11:17:42 -0700

    Using the loadData function

--- R/concatenate-files.R
+++ R/concatenate-files.R
@@ -1,30 +1,6 @@
 library(tools)
+source("common.R")
 
-resultsFolder = "../../results"
-
-files <<- list.files(path=resultsFolder, pattern="*.csv", full.names=T, recursive=FALSE)
-
-data <- data.frame(PROJECT = character(0),
-                   SHA = character(0),
-                   FILE = character(0),
-                   LOC_A_TO_B = integer(0),
-                   LOC_A_TO_SOLVED = integer(0),
-                   LOC_B_TO_SOLVED = integer(0),
-                   AST_A_TO_B = integer(0),
-                   AST_A_TO_SOLVED = integer(0),
-                   AST_B_TO_SOLVED = integer(0))
-
-lapply(files, function(file) {
-  fileLength = length(readLines(file))
-  if (fileLength <= 2)
-    return 
-  else {
-    fileInfo <- file.info(file)
-    currentDataFile <- read.csv(file, header=T, sep=',', blank.lines.skip=T, as.is=T)
-    project <- basename(file_path_sans_ext(file))
-    currentDataFile$PROJECT <- project
-    data <<- rbind(data, currentDataFile)
-  }
-})
+data <- loadData(resultsFolder)
 
 write.csv(data, file=paste(resultsFolder, "/all.csv", sep=''), row.names=FALSE)
