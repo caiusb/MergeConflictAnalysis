eedc8572cb0ebce13c147a3bc9b1fd87de755341
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2015-06-22 13:33:18 -0700

    More time functions

--- R/time-metrics.R
+++ R/time-metrics.R
@@ -7,6 +7,16 @@ if(exists("SECONDS_PER_DAY") != TRUE) {
   lockBinding("SECONDS_PER_DAY", globalenv())
 }
 
+if(exists("SECONDS_PER_HOUR") != TRUE) {
+  SECONDS_PER_HOUR <- 3600
+  lockBinding("SECONDS_PER_HOUR", globalenv())
+}
+
+if(exists("SECONDS_PER_MINUTE") != TRUE) {
+  SECONDS_PER_MINUTE <- 60
+  lockBinding("SECONDS_PER_MINUTE", globalenv())
+}
+
 calculateTimeDifferences <<- function(data) {
   data <- calculateResolutionTime(data)  
   data <- calculateTimeBetweenTips(data)
@@ -28,18 +38,25 @@ calculateEffort <- function(data) {
   weightedAverageConflictSize <- (weightedConflictSize)/2
   averageConflictSize <- (data$LOC_A_TO_SOLVED + data$LOC_B_TO_SOLVED)/2
   deviationFromDiagonal <- (data$LOC_B_TO_SOLVED-data$LOC_A_TO_SOLVED)/sqrt(2)
-  effort <- deviationFromDiagonal*weightedAverageConflictSize
+  effort <- averageConflictSize
   return(effort)
-
 }
 
-trimSolveTimeGreaterThanDays <-function(data, days){
-  return(data[data$RESOLUTION_TIME <= days*SECONDS_PER_DAY, ]);
+trimSolveTimeGreaterThanDays <-function(data, days) {
+  return(data[data$RESOLUTION_TIME <= days*SECONDS_PER_DAY, ])
+}
+
+trimSolveTimeGreaterThanHours <- function(data, hours) {
+  return(data[data$RESOLUTION_TIME <= hours*SECONDS_PER_HOUR, ])
+}
+
+trimSolveTimeGreaterThanMinutes <- function(data, minutes) {
+  return(data[data$RESOLUTION_TIME <= minutes*SECONDS_PER_MINUTE, ])
 }
 
 timedData <- calculateTimeDifferences(data)
 timedCommitData <- calculateTimeDifferences(createCommitData(timedData))
-trimmedCommitData <- trimSolveTimeGreaterThanDays(timedCommitData, 2)
+trimmedCommitData <- trimSolveTimeGreaterThanMinutes(timedCommitData, 10)
 trimmedCommitData$EFFORT <- calculateEffort(trimmedCommitData)
 print(summary(trimmedCommitData$RESOLUTION_TIME))
 cat("Standard deviation: ", sd(trimmedCommitData$RESOLUTION_TIME))
