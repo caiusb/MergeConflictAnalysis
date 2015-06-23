71bfb6c7373fe556153461d54a30f47b62eade53
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2015-06-23 15:30:06 -0700

    Moved some functionality to a new class

--- MergeConflictAnalysis/src/edu/oregonstate/mergeproblem/mergeconflictanalysis/CommitStatus.java
+++ MergeConflictAnalysis/src/edu/oregonstate/mergeproblem/mergeconflictanalysis/CommitStatus.java
@@ -1,47 +1,43 @@
 package edu.oregonstate.mergeproblem.mergeconflictanalysis;
 
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.eclipse.jgit.lib.Repository;
 
 public class CommitStatus {
 	private Repository repository;
 	private String sha1;
-	private Map<String, CombinedFile> combinedFiles;
-	private Map<String, String> solvedVersions;
 	private int time;
-	private List<String> conflictingFiles;
-	private List<String> modifiedFiles = null;
+	
+	private Map<String, FileStatus> fileStatuses = new HashMap<String, FileStatus>();
 	
 	@SuppressWarnings("resource")
 	public CommitStatus(Repository repository, String sha1, Map<String, CombinedFile> conflictingFiles, int time) {
 		this.repository = repository;
 		this.sha1 = sha1;
-		this.combinedFiles = conflictingFiles;
-		this.solvedVersions = new HashMap<String, String>(conflictingFiles.size());
-		this.conflictingFiles = new ArrayList<>();
-		this.conflictingFiles.addAll(conflictingFiles.keySet());
 		this.time = time;
-		for (String file : this.conflictingFiles) {
-			String fileContents = Util.retrieveFile(repository, sha1, file);
-			solvedVersions.put(file, fileContents);
+		
+		for (String file : conflictingFiles.keySet()) {
+			FileStatus filestatus = new FileStatus(this, file, conflictingFiles.get(file));
+			fileStatuses.put(file, filestatus);
 		}
 	}
 	
 	public List<String> getListOfConflictingFiles() {
-		return conflictingFiles;
+		return fileStatuses.values().stream().filter(status -> status.isConflicting())
+				.map((status) -> status.getFileName()).collect(Collectors.toList());
 	}
 	
 	public String getSolvedVersion(String fileName) {
-		return solvedVersions.get(fileName);
+		return fileStatuses.get(fileName).getSolvedVersion();
 	}
 	
 	public CombinedFile getCombinedFile(String fileName) {
-		CombinedFile combinedFile = combinedFiles.get(fileName);
-		return combinedFile;
+		return fileStatuses.get(fileName).getCombinedFile();
 	}
 	
 	public String getSHA1() {
@@ -57,23 +53,8 @@ public class CommitStatus {
 	}
 	
 	public List<String> getModifiedFiles() {
-		return modifiedFiles;
-	}
-
-	private void initModifiedFiles() {
-		modifiedFiles = Util.getFilesChangedByCommit(repository, sha1);
-		System.out.println("Files modified for " + sha1 + ":");
-		for (String file : modifiedFiles) {
-			System.out.println(file);
-		}
-		System.out.println("====");
-		for (String file : modifiedFiles) {
-			if (conflictingFiles.contains(file))
-				continue;
-			String contents = Util.retrieveFile(repository, sha1, file);
-			CombinedFile combinedFile = new CombinedFile();
-			combinedFile.addChunk(ChunkOwner.BOTH, contents);
-			combinedFiles.put(file, combinedFile);
-		}
+		List<String> list = Collections.emptyList();
+		list.addAll(fileStatuses.keySet());
+		return list;
 	}
 }
---
+++ MergeConflictAnalysis/src/edu/oregonstate/mergeproblem/mergeconflictanalysis/FileStatus.java
@@ -0,0 +1,38 @@
+package edu.oregonstate.mergeproblem.mergeconflictanalysis;
+
+public class FileStatus {
+	
+	private CommitStatus parentCommitStatus;
+	private CombinedFile file;
+	private boolean isConflicting;
+	private String name;
+	private String solvedVersion;
+
+	public FileStatus(CommitStatus parentCommitStatus, String name, CombinedFile file) {
+		this(parentCommitStatus, name, file, true);
+	}
+	
+	public FileStatus(CommitStatus parentCommitStatus, String name, CombinedFile file, boolean isConflicting) {
+		this.parentCommitStatus = parentCommitStatus;
+		this.name = name;
+		this.file = file;
+		this.isConflicting = isConflicting;
+		this.solvedVersion = Util.retrieveFile(parentCommitStatus.getRepository(), parentCommitStatus.getSHA1(), name);
+	}
+	
+	public String getFileName() {
+		return name;
+	}
+	
+	public String getSolvedVersion() {
+		return solvedVersion;
+	}
+
+	public boolean isConflicting() {
+		return isConflicting;
+	}
+
+	public CombinedFile getCombinedFile() {
+		return file;
+	}
+}
