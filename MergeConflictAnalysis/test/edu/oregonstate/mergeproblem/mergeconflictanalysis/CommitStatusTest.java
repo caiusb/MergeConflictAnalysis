5741f9067738c2c255f118998692eb173f1e1aad
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2015-06-23 15:12:29 -0700

    A test (ignored for now)

---
+++ MergeConflictAnalysis/test/edu/oregonstate/mergeproblem/mergeconflictanalysis/CommitStatusTest.java
@@ -0,0 +1,22 @@
+package edu.oregonstate.mergeproblem.mergeconflictanalysis;
+
+import java.util.Arrays;
+
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class CommitStatusTest extends MergeGitTest {
+	
+	@Test
+	@Ignore
+	public void testGetCombinedFileOfNonConflictingFile() throws Exception {
+		createConflictingMergeResult();
+		RevCommit merge = add(Arrays.asList(new String[]{"A.java", "B.java"}), Arrays.asList(new String[]{"resolved", "something"}));
+		InMemoryMerger merger = new InMemoryMerger(repository);
+		CommitStatus commitStatus = merger.recreateMerge(merge);
+		CombinedFile combinedFile = commitStatus.getCombinedFile("B.java");
+		assertNotNull(combinedFile);
+	}
+
+}
