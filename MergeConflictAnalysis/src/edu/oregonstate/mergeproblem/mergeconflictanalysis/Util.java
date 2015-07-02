c30515cf1065bd48ee92dfdb76a0b16731635f30
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2015-07-02 13:39:02 -0700

    Added method to get the modified files
    Finally, it works!

--- MergeConflictAnalysis/src/edu/oregonstate/mergeproblem/mergeconflictanalysis/Util.java
+++ MergeConflictAnalysis/src/edu/oregonstate/mergeproblem/mergeconflictanalysis/Util.java
@@ -1,9 +1,15 @@
 package edu.oregonstate.mergeproblem.mergeconflictanalysis;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.diff.DiffEntry;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.ObjectReader;
 import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.treewalk.CanonicalTreeParser;
 import org.gitective.core.BlobUtils;
 import org.gitective.core.GitException;
 
@@ -21,7 +27,28 @@ public class Util {
 		return content;
 	}
 	
-	public static List<String> getFilesChangedByCommit(Repository repository, String sha1) {
-		return new ArrayList<String>();
-	}
+	public static Map<String, String> getFilesChangedByCommit(Repository repository, String sha1) {
+		Map<String, String> changedFiles = new HashMap<String, String>();
+		try {
+			ObjectId head = repository.resolve("HEAD^{tree}");
+			ObjectId oldHead = repository.resolve("HEAD~1^{tree}");
+			
+			ObjectReader reader = repository.newObjectReader();
+			CanonicalTreeParser oldTreeIter = new CanonicalTreeParser();
+			oldTreeIter.reset(reader, oldHead);
+			CanonicalTreeParser newTreeIter = new CanonicalTreeParser();
+			newTreeIter.reset(reader, head);
+			
+			Git git = new Git(repository);
+			List<DiffEntry> diffEntries = git.diff().setNewTree(newTreeIter).setOldTree(oldTreeIter).call();
+			git.close();
+			
+			for (DiffEntry entry : diffEntries) {
+				String path = entry.getNewPath();
+				changedFiles.put(path, retrieveFile(repository, sha1, path));
+			}
+		} catch (Exception e) {}
+		
+		return changedFiles;
+	} 
 }
--- MergeConflictAnalysis/test/edu/oregonstate/mergeproblem/mergeconflictanalysis/UtilTest.java
+++ MergeConflictAnalysis/test/edu/oregonstate/mergeproblem/mergeconflictanalysis/UtilTest.java
@@ -1,20 +1,18 @@
 package edu.oregonstate.mergeproblem.mergeconflictanalysis;
 
 import java.util.Arrays;
-import java.util.List;
+import java.util.Map;
 
 import org.eclipse.jgit.revwalk.RevCommit;
-import org.junit.Ignore;
 import org.junit.Test;
 
 public class UtilTest extends MergeGitTest {
 
 	@Test
-	@Ignore
 	public void testAddFileChangeInMerge() throws Exception {
 		createConflictingMergeResult();
 		RevCommit mergeCommit = add(Arrays.asList(new String[]{"A.java", "second.java"}), Arrays.asList(new String[]{"Solved version", "Something else"}));
-		List<String> filesChangedByCommit = Util.getFilesChangedByCommit(repository, mergeCommit.getName());
-		assertEquals(2, filesChangedByCommit.size());
+		Map<String, String> filesChangedByCommit = Util.getFilesChangedByCommit(repository, mergeCommit.getName());
+		assertEquals(2, filesChangedByCommit.keySet().size());
 	}
 }
