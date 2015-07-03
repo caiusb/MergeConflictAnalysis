32c9ac9820f9561f4ce3a0758b7ea87d21195865
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2015-07-03 12:27:48 -0700

    Added a sanity test
    A way of validating a few assumptions

--- MergeConflictAnalysis/test/edu/oregonstate/mergeproblem/mergeconflictanalysis/UtilTest.java
+++ MergeConflictAnalysis/test/edu/oregonstate/mergeproblem/mergeconflictanalysis/UtilTest.java
@@ -15,4 +15,21 @@ public class UtilTest extends MergeGitTest {
 		Map<String, String> filesChangedByCommit = Util.getFilesChangedByCommit(repository, mergeCommit.getName());
 		assertEquals(2, filesChangedByCommit.keySet().size());
 	}
+	
+	@Test
+	public void testFileChanged() throws Exception {
+		add("A.java", "something");
+		branch("branch");
+		add("A.java", "something else");
+		add("B.java", "bla");
+		add("C.java", "bla bla");
+		checkout("master");
+		add("A.java", "conflicting");
+		add("D.java", "something else");
+		merge("branch");
+		RevCommit merge = add("A.java", "conflict resolution");
+		assertEquals(2, merge.getParentCount());
+		Map<String, String> filesChanged = Util.getFilesChangedByCommit(repository, merge.getName());
+		assertEquals(1, filesChanged.size());
+	}
 }
