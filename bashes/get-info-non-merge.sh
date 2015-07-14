c026aa01073b8925621a970403ffbe2ffe16f46e
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2015-07-14 16:21:31 -0700

    Added script to get info about regular commits

---
+++ bashes/get-info-non-merge.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+source common.sh
+
+folder=$(resolve-path $1)
+results=$(resolve-path $2)
+orderfile=$folder/'order.txt'
+
+pushd $folder > /dev/null
+
+for i in *
+do
+	if [ -f $i ]
+	then
+		continue
+	fi
+	pushd $i > /dev/null
+	if [ -e '.git' ]
+	then
+		resultfile=$results/$(basename $i)
+		echo "SHA,COMMIT_TIME,AUTHOR" > $resultfile
+		git log --max-parents=1 --format="%H,%at,%ae" > $resultfile
+	fi
+	popd > /dev/null
+done
+
+popd > /dev/null
