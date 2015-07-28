ded6bcbec86930c95087d7c0b88c07d15065ce79
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2015-07-28 12:39:52 -0700

    Getting the pull request info

--- github/get-pull-request-info.py
+++ github/get-pull-request-info.py
@@ -2,6 +2,38 @@
 
 import requests as req
 
-r = req.get('https://api.github.com')
+username = 'caiusb'
+passwordFile = 'token'
+reposFile = 'repos.txt'
+root = 'https://api.github.com/repos/'
 
-print(r.status_code)
+def getAuthToken(file):
+	f = open(file, 'r')
+	password = f.read()[:-1]
+	f.close()
+	return password
+
+def getRepos(file):
+	f = open(file, 'r')
+	string = f.read()
+	f.close()
+
+	listOfRepos = string.split('\n')
+	repos = []
+	for repo in listOfRepos:
+		if (repo == ''):
+			continue
+		rs = repo.split(',')
+		repos.append({'username': rs[0], 'repo': rs[1]})
+
+	return repos
+
+password = getAuthToken(passwordFile)
+repos = getRepos(reposFile)
+
+for repo in repos:
+	apiCall = root + repo['username'] + '/' + repo['repo'] + '/pulls'
+	r = req.get(apiCall, auth=(username, password))
+	print(r.status_code)
+	print(r.text)
+
