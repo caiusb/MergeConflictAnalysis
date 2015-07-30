e601d2900f956cce3e8d46a6b88f44ce5030790d
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2015-07-29 17:06:35 -0700

    Simplified the script
    The resolve-path thing give me something absolute

--- bashes/group-pr-summaries.sh
+++ bashes/group-pr-summaries.sh
@@ -5,15 +5,11 @@ source common.sh
 srcfolder=$(resolve-path $1)
 dstfolder=$(resolve-path $2)
 
-pushd $dstfolder > /dev/null
-	dstabs=`pwd`
-popd > /dev/null
-
 pushd $srcfolder > /dev/null
 
 for i in *
 do
-	mv $i/summary.csv $dstabs/$i'.csv'
+	mv $i/summary.csv $dstfolder/$i'.csv'
 done
 
 popd > /dev/null
