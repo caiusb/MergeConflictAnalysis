53e7f16659205de4e11f22803746167711d93d14
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2015-08-04 22:39:47 -0700

    Basic pull request histogram

---
+++ R/pull-req.R
@@ -0,0 +1,8 @@
+source('common.R')
+
+pullReq <- loadPullReqData(prFolder)
+
+mergedPullReq <- pullReq[pullReq$MERGED == 'True', ]
+mergedPullReq$SOLVE_TIME <- mergedPullReq$MERGED_TIME - mergedPullReq$CREATED_TIME
+trimmed <- mergedPullReq[mergedPullReq$SOLVE_TIME <= 7*60*60, ] #7 days solve time
+hist(trimmed$SOLVE_TIME, breaks=50)
