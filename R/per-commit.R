505a7576e49df71d53bef7614b93930671bff271
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2015-08-04 22:39:27 -0700

    Generating per commit data

---
+++ R/per-commit.R
@@ -0,0 +1,16 @@
+source('common.R')
+
+data <- loadData(resultsFolder)
+commitData <- createCommitData(data)
+
+factors <- levels(factor(commitData$PROJECT))
+frames <- split(commitData, commitData$PROJECT)
+
+commitFolder <- "../../results/per-commit/"
+
+lapply(factors, function(factor) {
+  file <- concat(concat(commitFolder, as.character(factor)), ".csv")
+  print(file)
+  project <- frames[[factor]]
+  write.csv(project, file, sep='')
+})
