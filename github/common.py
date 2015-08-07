8eae49f730d1947c30a7255388b4c7472c34ea40
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2015-08-07 12:57:05 -0700

    Added method do deal with API rate limits

--- github/common.py
+++ github/common.py
@@ -1,12 +1,23 @@
 #!/opt/local/bin/python
 
 import os
+import requests as req
 
 username = 'caiusb'
 passwordFile = 'token'
 reposFile = 'repos.txt'
 root = 'https://api.github.com/repos/'
 results = '../../results'
+
+def doApiCall(url, auth, params={}):
+	resp = req.get(url, auth=auth, params=params)
+	if (resp.status_code == 403):
+		while (resp.headers['X-RateLimit-Remaining'] == 0):
+			sleepTime = r.headers['X-RateLimit-Reset']
+			print('Exhausted the API Rate Limit. Sleeping for ' + str(sleepTime))
+			sleep(sleepTime)
+			resp = req.get(url, auth=auth, params=params)
+	return resp.text
 
 def getUsername():
 	return username
--- github/get-pull-request-info.py
+++ github/get-pull-request-info.py
@@ -8,15 +8,15 @@ import os
 def getCommitsForPullReq(jsonPull, auth):
 	number = pull['number']
 	url = pull['_links']['commits']['href']
-	resp = req.get(url, auth=auth)
-	return resp.text
+	resp = c.doApiCall(url, auth)
+	return resp
 
 def getEventsForPullReq(jsonPull, auth):
 	number = pull['number']
 	issueURL = pull['_links']['issue']['href']
 	eventsURL = issueURL + '/events'
-	resp = req.get(eventsURL, auth=auth)
-	return resp.text
+	resp = c.doApiCall(eventsURL, auth=auth)
+	return resp
 
 username = c.getUsername()
 password = c.getAuthToken()
@@ -30,10 +30,7 @@ for repo in repos:
 	apiCall = repoRoot + '/pulls'
 	params = {'state': 'all'}
 	auth = (username, password)
-	resp = req.get(apiCall, auth=auth, params=params)
-	if (resp.status_code != 200):
-		print('Error getting data for ' + repoName)
-	text = resp.text
+	text = c.doApiCall(apiCall, auth=auth, params=params)
 	listOfPulls = json.loads(text)
 	os.mkdir(results + "/" + repoName)
 	for pull in listOfPulls:
