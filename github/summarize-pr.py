8aa34a9837754f8eb56b1d57ca373000c2b22b78
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2015-08-17 12:41:26 -0700

    Added a mergeable field

--- github/summarize-pr.py
+++ github/summarize-pr.py
@@ -16,7 +16,7 @@ def getProjectData(folder):
 	pulls = json.loads(pullContent)
 	with open(folder + '/summary.csv', 'w') as csvFile:
 		writer = csv.writer(csvFile, delimiter=',', quoting=csv.QUOTE_MINIMAL)
-		writer.writerow(['PR', 'MERGED', 'CLOSED', 'CREATED_TIME', 'MERGED_TIME', 'SHA'])
+		writer.writerow(['PR','MERGEABLE', 'MERGED', 'CLOSED', 'CREATED_TIME', 'MERGED_TIME', 'SHA'])
 		processPullRequests(folder, pulls, writer)
 		
 def processPullRequests(folder, pulls, csvWriter):
@@ -24,23 +24,28 @@ def processPullRequests(folder, pulls, csvWriter):
 		number = pull['number']
 		eventsFile = str(number) + '.events.json'
 		commitsFile = str(number) + '.commits.json'
+		fullFile = str(number) + '.full.json'
 		with open(folder + '/' + eventsFile, 'r') as f:
 			eventsContent = f.read()
+		with open(folder + '/' + fullFile, 'r') as f:
+			fullPullContent = f.read()
 		events = json.loads(eventsContent)
+		fullPull = json.loads(fullPullContent)
 		mergeTime = 0
 		createTime = 0
 		merged = False
-		closed = (pull['state'] == 'closed')
+		closed = (fullPull['state'] == 'closed')
 		sha = ''
+		mergeable = fullPull['mergeable']
 		for event in events:
 			if (event['event'] == 'merged'):
 				mergeDate = event['created_at']
 				mergeTime = convertDateToUNIX(mergeDate)
-				createDate = pull['created_at']
+				createDate = fullPull['created_at']
 				createTime = convertDateToUNIX(createDate)
 				merged = True
 				sha = event['commit_id']
-		csvWriter.writerow([str(number), str(merged), str(closed), str(createTime), str(mergeTime), sha])
+		csvWriter.writerow([str(number), str(mergeable), str(merged), str(closed), str(createTime), str(mergeTime), sha])
 
 def convertDateToUNIX(date):
 	return datetime.datetime.strptime(date, '%Y-%m-%dT%H:%M:%SZ').strftime('%s')
