bb16ae2b3aa902baaa90be89c825efbddccb2f3d
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2015-08-18 09:07:22 -0700

    Merging the merge data and pull requests

--- R/pull-req.R
+++ R/pull-req.R
@@ -6,3 +6,8 @@ mergedPullReq <- pullReq[pullReq$MERGED == 'True', ]
 mergedPullReq$SOLVE_TIME <- mergedPullReq$MERGED_TIME - mergedPullReq$CREATED_TIME
 trimmed <- mergedPullReq[mergedPullReq$SOLVE_TIME <= 7*60*60, ] #7 days solve time
 hist(trimmed$SOLVE_TIME, breaks=50)
+mergePullRequestAndCommitData <- function(commitData, pullReqData) {
+  return(merge(commitData, pullReqData, by.x = "SHA", by.y = "SHA", all.x = TRUE, all.y = FALSE))
+}
+
+merged <<- mergePullRequestAndCommitData(commitData, pullReq)
