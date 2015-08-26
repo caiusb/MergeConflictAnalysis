50e2521b6ae54dd9c1dfac60d596b4ddae01ca29
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2015-08-25 23:52:26 -0700

    Stuff

--- R/pull-req.R
+++ R/pull-req.R
@@ -1,13 +1,14 @@
 source('common.R')
 
+commitData <- loadCommitData(commitFolder)
 pullReq <- loadPullReqData(prFolder)
 
 mergedPullReq <- pullReq[pullReq$MERGED == TRUE, ]
 mergedPullReq$SOLVE_TIME <- mergedPullReq$MERGED_TIME - mergedPullReq$CREATED_TIME
-trimmed <- mergedPullReq[mergedPullReq$SOLVE_TIME <= 7*60*60, ] #7 days solve time
-hist(trimmed$SOLVE_TIME, breaks=50)
+#trimmed <- mergedPullReq[mergedPullReq$SOLVE_TIME <= 7*60*60, ] #7 days solve time
+#hist(trimmed$SOLVE_TIME, breaks=50)
 mergePullRequestAndCommitData <- function(commitData, pullReqData) {
-  return(merge(commitData, pullReqData, by.x = "SHA", by.y = "SHA", all.x = TRUE, all.y = FALSE))
+  return(merge(commitData, pullReqData, by = c("SHA"), all.x = TRUE, all.y = FALSE))
 }
 
-merged <<- mergePullRequestAndCommitData(commitData, pullReq)
+merged <<- mergePullRequestAndCommitData(commitData, mergedPullReq)
