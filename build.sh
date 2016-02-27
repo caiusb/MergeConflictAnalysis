3ce2851f67717778a5d1006db3180a434c3aace9
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2016-02-26 19:05:57 -0800

    Correct build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd MergeConflictAnalysis
-M2_HOME='mvn --version | grep "Maven home" | rev | cut -d':' -f1 | rev | sed "s/^ *//"'
+export M2_HOME=`mvn --version | grep "Maven home" | rev | cut -d':' -f1 | rev | sed "s/^ *//"`
 sbt clean compile test
