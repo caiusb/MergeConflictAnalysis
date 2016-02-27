c4d29ec4a19b6fdcff87535d32550f2a479054ba
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2016-02-27 12:21:33 -0800

    Fixed the MergeBuilderTest

--- MergeConflictAnalysis/src/test/scala/edu/oregonstate/mergeproblem/mergeconflictanalysis/build/MergeBuilder.scala
+++ MergeConflictAnalysis/src/test/scala/edu/oregonstate/mergeproblem/mergeconflictanalysis/build/MergeBuilder.scala
@@ -1,19 +1,23 @@
 package edu.oregonstate.mergeproblem.mergeconflictanalysis.build
 
 import edu.oregonstate.mergeproblem.mergeconflictanalysis.MergeGitTest
-import org.scalatest.{Matchers, FlatSpecLike}
+import org.scalatest.{BeforeAndAfter, Matchers, FlatSpecLike}
 
 import scala.io.Source
 
-class MergeBuilder extends MergeGitTest with FlatSpecLike with Matchers {
+class MergeBuilder extends MergeGitTest with FlatSpecLike with Matchers with BeforeAndAfter {
+
+  before {
+    setUp()
+  }
 
   def getPOMContent: String =
-    Source.fromFile(getClass.getResource("example.pom.xml").getFile).mkString
+    Source.fromFile(getClass.getResource("/example.pom.xml").getFile).mkString
 
   it should "correctly merge a simple example" in {
     add(testRepo, "pom.xml", getPOMContent)
     add(testRepo, "src/main/java/A.java", "public class A{\npublic int x{\nreturn 0;\n}}")
-    checkout("branch")
+    branch("branch")
     add(testRepo, "src/main/java/A.java", "public class A{\npublic int x{\return 0;\n}\npublic void n(){\n}}")
     checkout("master")
     add(testRepo, "scr/main/java/A.java", "public class A{\npublic int x{\nreturn 1;\n}}")
