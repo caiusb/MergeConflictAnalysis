c5e7c61087a958b0508a20b5db71b0592c3fe3fb
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2016-02-27 19:42:20 -0800

    IDEA screwed up the refactoring
    Again...

--- MergeConflictAnalysis/src/main/java/edu/oregonstate/mergeproblem/mergeconflictanalysis/AbstractAnalysis.java
+++ MergeConflictAnalysis/src/main/java/edu/oregonstate/mergeproblem/mergeconflictanalysis/AbstractAnalysis.java
@@ -4,9 +4,6 @@ import edu.oregonstate.mergeproblem.mergeconflictanalysis.Main;
 
 import java.io.BufferedOutputStream;
 
-/**
- * Created by caius on 2/27/16.
- */
 public abstract class AbstractAnalysis {
-    abstract void doAnalysis(Main.Config config, BufferedOutputStream outputStream) throws Exception;
+    abstract public void doAnalysis(Main.Config config, BufferedOutputStream outputStream) throws Exception;
 }
