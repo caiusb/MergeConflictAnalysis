05c631017523712c545dea8155dc2dd3a4822e6b
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2016-02-27 19:46:32 -0800

    A brand new build analysis

---
+++ MergeConflictAnalysis/src/main/scala/edu/oregonstate/mergeproblem/mergeconflictanalysis/build/BuildAnalysis.scala
@@ -0,0 +1,21 @@
+package edu.oregonstate.mergeproblem.mergeconflictanalysis.build
+
+import java.io.{File, BufferedOutputStream}
+
+import edu.oregonstate.mergeproblem.mergeconflictanalysis.{MergeFilter, RepositoryWalker, Main, AbstractAnalysis}
+import org.eclipse.jgit.api.Git
+
+import scala.collection.JavaConversions._
+
+object BuildAnalysis extends AbstractAnalysis {
+
+  override def doAnalysis(config: Main#Config, outputStream: BufferedOutputStream) = {
+    asScalaBuffer(config.repositories).foreach(r => {
+      val git = Git.open(new File(r))
+      val repo = git.getRepository
+      val mergeCommits = asScalaBuffer(new RepositoryWalker(repo).getMergeCommits)
+      val result = mergeCommits.toParArray.map(c => MergeBuilder.mergeAndBuild(git, c.getName) + "\n").reduce((c, e) => (c + e))
+      outputStream.write(result.getBytes)
+    })
+  }
+}
