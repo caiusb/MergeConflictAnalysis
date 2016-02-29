a71710926d4c4defcfa51da11eff08c161ab9b3b
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2016-02-28 19:01:49 -0800

    Making the scripts for the submit task

---
+++ cluster/make-dir-structure.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ ! -d "scripts" ]
+then
+	mkdir "scripts"
+fi
+
+if [ ! -d "../../build-data" ] 
+then
+	mkdir "../../build-data"
+fi
+
+if [ ! -d "../../build-data/output" ]
+then
+	mkdir "../../build-data/output"
+fi
+
+if [ ! -d "../../build-data/errors" ]
+then
+	mkdir "../../build-data/errors"
+fi
+
+if [ ! -d "../../build-data/results" ]
+then
+	mkdir "../../build-data/results"
+fi
---
+++ cluster/make-scripts.py
@@ -0,0 +1,27 @@
+#!~/.local/bin/python
+
+import os
+import subprocess as s
+
+repos = [line.rstrip('\n') for line in open('../repos-buildable.txt')]
+
+shebang = "#!/bin/bash\n\n"
+jar = "$HOME/merging/Merge"
+jobOps = "-cwd"
+
+s.call("./make-dir-structure.sh")
+
+for repo in repos:
+	repoName = repo.split("/")[-1]
+	with open("scripts/" + repoName + ".sh", "w") as script:
+		script.write(shebang)
+		script.write("tmpdir=\"/scratch/brindesc\"\n\n")
+		script.write("if [ ! -d \"$tmpdir\" ]\n")
+		script.write("then\n")
+		script.write("\tmkdir \"$tmpdir\"\n")
+		script.write("fi\n\n")
+		script.write("pushd \"$tmpdir\" > /dev/null \n")
+		script.write("git clone " + repo)
+		script.write("popd >/dev/null \n\n")
+		script.write("java -jar " + jar + " -Xmx1G " + "-output=../../build-data/results/" + repoName + ".csv " + "/scratch/brindesc/" + repoName + "\n\n")
+		script.write("rm -rf \"/$tmpdir\"\n\n")
