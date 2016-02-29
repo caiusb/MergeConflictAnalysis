0615e6ea44b4c80a931d32c33b905a5eb027e4a9
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2016-02-28 23:04:33 -0800

    Forgot a slash

--- cluster/make-scripts.py
+++ cluster/make-scripts.py
@@ -29,5 +29,5 @@ for repo in repos:
 		script.write("pushd \"$tmpdir\" > /dev/null \n")
 		script.write("git clone " + repo + "\n")
 		script.write("popd >/dev/null \n\n")
-		script.write("java -jar " + jar + " -Xmx2G " + "-output=../build-data/results/" + repoName + ".csv -build -log-to-console " + "$tmpdir" + repoName + "\n\n")
+		script.write("java -jar " + jar + " -Xmx2G " + "-output=../build-data/results/" + repoName + ".csv -build -log-to-console " + "$tmpdir/" + repoName + "\n\n")
 		script.write("rm -rf \"$tmpdir\"\n\n")
