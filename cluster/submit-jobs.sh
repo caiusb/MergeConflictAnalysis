5496d2295cfe63c98042a5bf60e9fb21c4cc322f
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2016-02-28 19:16:15 -0800

    Script for submitting the scripts

---
+++ cluster/submit-jobs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+touch jobs.txt
+
+cd "scripts"
+
+for i in *
+do
+	basename=`echo $i | rev | cut -d"." -f2 | rev`
+	qsub -cwd -S /bin/bash -o "../../../build-data/output/$basename" -e "../../../build-data/error/$basename" $i >> ../jobs.txt
+done
