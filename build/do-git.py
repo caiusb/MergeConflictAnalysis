ce7007179b138ae33932a282b1df0de55eeead7e
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2016-03-09 14:33:38 -0800

    Made it work

--- build/do-git.py
+++ build/do-git.py
@@ -4,36 +4,41 @@ import subprocess as s
 import os
 import git as g
 
-gitFolder = '../'#'/scratch/brindesc/git'
+gitFolder = '/scratch/brindesc/git'
 
-testCommand = 'make test'
+testCommand = ['make', 'test']
 buildCommand = 'make'
-cleanCommand = 'make clean'
+cleanCommand = ['make', 'clean']
+
+FNULL = open(os.devnull, 'w')
 
 class BuildResult:
 	def __init__(self, parent1, parent2, merge):
 		self.parent1 = parent1
 		self.parent2 = parent2
-		self.parent3 = parent3
+		self.merge = merge
+
+	def __str__(self):
+		return self.getResult()
 
 	def getResult(self):
-		return self.parent1 + ',' + self.parent2 + ',' + self.parent3
+		return self.parent1 + ',' + self.parent2 + ',' + self.merge
 
 def buildAsIs(hexsha, repo):
 	repo.git.checkout(hexsha)
-	s.call(cleanCommand)
-	build
+	return build()
 
 def build():
-	if (s.call(buildCommand) != 0)
-			return "build"
-		elif (s.call(testCommand) != 0):
-			return "test"
-		else:
-			return "pass"
+	s.call(cleanCommand, stdout=FNULL, stderr=FNULL)
+	if (s.call(buildCommand, stdout=FNULL, stderr=FNULL) != 0):
+		return "build"
+	elif (s.call(testCommand, stdout=FNULL, stderr=FNULL) != 0):
+		return "test"
+	else:
+		return "pass"
 
-builds = {}
 results = {}
+builds = {}
 
 os.chdir(gitFolder)
 
@@ -42,19 +47,26 @@ commits = repo.iter_commits(repo.branches.master)
 merges = [c for c in commits if len(c.parents) == 2]
 
 for m in merges:
+	print('Testing ' + m.hexsha + " [" +  str(merges.index(m)) + "/" + str(len(merges)) + "]")
 	hexsha = m.hexsha
 	p1 = m.parents[0].hexsha
 	p2 = m.parents[1].hexsha
 	if (not p1 in builds):
-		builds[p1]=buildAsIs(p1)
+		builds[p1]=buildAsIs(p1, repo)
 	if (not p2 in builds):
-		builds[p2]=buildAsIs(p2)
-	s.call(cleanCommand)
+		builds[p2]=buildAsIs(p2, repo)
+	s.call(cleanCommand, stdout=FNULL, stderr=FNULL)
 	repo.git.checkout(p2)
 	try:
 		repo.git.merge(p1)
 	except GitCommandError:
-		merges[hexsha] = BuildResult(builds[p1], builds[p2], "text")
+		results[hexsha] = BuildResult(builds[p1], builds[p2], "text")
 		continue
-	merges[hexsha] = BuildResult(builds[p1], builds[2], build)
-	r.git.checkout(".", f=True)
+	results[hexsha] = BuildResult(builds[p1], builds[p2], build)
+	print(results[hexsha])
+	print(type(builds[p1]))
+	repo.git.checkout(".", f=True)
+	with open(os.path.expanduser("~/merging/build-data/git.csv"), "w+") as f:
+		f.write(hexsha + "," + builds[p1] + "," + builds[p2] + "," + results[hexsha] + "\n")
+
+print(merges)
