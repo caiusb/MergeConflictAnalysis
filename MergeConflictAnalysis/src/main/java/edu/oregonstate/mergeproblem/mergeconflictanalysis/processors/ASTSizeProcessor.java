69aa1d5d7d483ccf9d392e1c78f26accb9bca0cd
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2016-04-03 15:38:05 -0700

    The AST size is NA when it cannot be parsed

--- MergeConflictAnalysis/src/main/java/edu/oregonstate/mergeproblem/mergeconflictanalysis/processors/ASTSizeProcessor.java
+++ MergeConflictAnalysis/src/main/java/edu/oregonstate/mergeproblem/mergeconflictanalysis/processors/ASTSizeProcessor.java
@@ -4,6 +4,7 @@ import edu.oregonstate.mergeproblem.mergeconflictanalysis.file.ASTDiff;
 import edu.oregonstate.mergeproblem.mergeconflictanalysis.file.ChunkOwner;
 import edu.oregonstate.mergeproblem.mergeconflictanalysis.file.CombinedFile;
 import edu.oregonstate.mergeproblem.mergeconflictanalysis.file.CommitStatus;
+import fr.labri.gumtree.tree.Tree;
 
 public class ASTSizeProcessor implements FileProcessor {
 
@@ -15,16 +16,23 @@ public class ASTSizeProcessor implements FileProcessor {
 	@Override
 	public String getData(CommitStatus status, String fileName) {
 		CombinedFile combinedFile = status.getCombinedFile(fileName);
-		int aSize = -1;
-		int bSize = -1;
-		int solvedSize = -1;
+		String aSize = "NA";
+		String bSize = "NA";
+		String solvedSize = "NA";
 		ASTDiff astDiff = new ASTDiff();
 
-		aSize = astDiff.getTree(combinedFile.getVersion(ChunkOwner.A)).getSize();
-		bSize = astDiff.getTree(combinedFile.getVersion(ChunkOwner.B)).getSize();
-		solvedSize = astDiff.getTree(status.getSolvedVersion(fileName)).getSize();
-		
+		aSize = getResultString(astDiff.getTree(combinedFile.getVersion(ChunkOwner.A)));
+		bSize = getResultString(astDiff.getTree(combinedFile.getVersion(ChunkOwner.B)));
+		solvedSize = getResultString(astDiff.getTree(status.getSolvedVersion(fileName)));
+
 		return aSize + "," + bSize + "," + solvedSize;
+	}
+
+	private String getResultString(Tree aTree) {
+		if(aTree.getChildren().size() != 0)
+			return aTree.getSize() + "";
+		else
+			return "NA";
 	}
 
 }
