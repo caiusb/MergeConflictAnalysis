ceada1eee26661807247655d646cce85b0b5c438
Caius Brindescu <brindesc@oregonstate.edu>
2016-04-02 17:26:26 -0700

    Rename

--- R/classify.R
+++ R/classify.R
@@ -3,6 +3,6 @@ library(dplyr)
 
 load("../../data/commitData")
 
-toClassify <- select(commitData, SHA, LOC_A_TO_B, LOC_A_TO_SOVED, LOC_B_TO_SOLVED, NO_STATEMENTS, NO_METHODS, NO_CLASSES)
+merge.classify.df <- select(commitData, SHA, LOC_A_TO_B, LOC_A_TO_SOVED, LOC_B_TO_SOLVED, NO_STATEMENTS, NO_METHODS, NO_CLASSES)
 
 #pam(toClassify, 10)
