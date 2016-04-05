f3ccdec6d63e29d5c6a1ac59686e51c01326e78b
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2016-04-05 14:42:22 -0700

    Formatting

--- MergeConflictAnalysis/src/main/java/edu/oregonstate/mergeproblem/mergeconflictanalysis/processors/OperationCounterProcessor.java
+++ MergeConflictAnalysis/src/main/java/edu/oregonstate/mergeproblem/mergeconflictanalysis/processors/OperationCounterProcessor.java
@@ -1,7 +1,5 @@
 package edu.oregonstate.mergeproblem.mergeconflictanalysis.processors;
 
-import java.util.List;
-
 import edu.oregonstate.mergeproblem.mergeconflictanalysis.file.ASTDiff;
 import edu.oregonstate.mergeproblem.mergeconflictanalysis.file.ChunkOwner;
 import edu.oregonstate.mergeproblem.mergeconflictanalysis.file.CombinedFile;
@@ -10,6 +8,8 @@ import fr.labri.gumtree.actions.model.Action;
 import fr.labri.gumtree.actions.model.Addition;
 import fr.labri.gumtree.actions.model.Delete;
 import fr.labri.gumtree.actions.model.Update;
+
+import java.util.List;
 
 public class OperationCounterProcessor implements FileProcessor {
 
@@ -23,7 +23,7 @@ public class OperationCounterProcessor implements FileProcessor {
 		int noAdd = 0;
 		int noUpdate = 0;
 		int noDelete = 0;
-		
+
 		CombinedFile combinedFile = status.getCombinedFile(fileName);
 		String aVersion = combinedFile.getVersion(ChunkOwner.A);
 		String bVersion = combinedFile.getVersion(ChunkOwner.B);
