43250b5b3c49c806051947c3f40a56df3f9687fe
Caius Brindescu <brindesc@oregonstate.edu>
2016-04-04 21:10:33 -0700

    Filtering and big memory

--- R/classify.R
+++ R/classify.R
@@ -1,8 +1,13 @@
 library(cluster)
 library(dplyr)
+library(bigmemory)
 
 load("../../data/commitData")
 
 merge.classify.df <- select(commitData, SHA, LOC_A_TO_B, LOC_A_TO_SOVED, LOC_B_TO_SOLVED, NO_STATEMENTS, NO_METHODS, NO_CLASSES)
+merge.classify <- merge.classify.df
+merge.classify$SHA <- NULL
+
+#merge.matrix <- as.big.matrix(dist(merge.classify))
 
 #pam(toClassify, 10)
