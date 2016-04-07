fbeac82c17c4ade732392bf73950034912203452
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2016-04-06 22:58:16 -0700

    New column names

--- stats/aggregate-data.py
+++ stats/aggregate-data.py
@@ -4,21 +4,28 @@ import os
 
 first = lambda x: x.iloc[0]
 
-groupDict = {'AST_A_BEFORE_SIZE': n.sum,
-             'LOC_A_BEFORE_SIZE': n.sum,
-             'AST_B_BEFORE_SIZE': n.sum,
-             'LOC_B_BEFORE_SIZE': n.sum,
-             'AST_A_TO_B': n.sum,
+def groupNodes(v):
+    allNodes = list()
+    for value in v.values.tolist():
+        nodes = value.split(';')
+        allNodes.extend(nodes)
+    return ';'.join(set(allNodes))
+
+groupDict = {'AST_A_SIZE': n.sum,
+             'LOC_A_SIZE': n.sum,
+             'AST_B_SIZE': n.sum,
+             'LOC_B_SIZE': n.sum,
+             'COMBINED_AST_A_TO_B': n.sum,
              'LOC_A_TO_B': n.sum,
-             'AST_A_TO_SOLVED': n.sum,
+             'COMBINED_AST_A_TO_SOLVED': n.sum,
              'LOC_A_TO_SOLVED': n.sum,
-             'AST_B_TO_SOLVED': n.sum,
+             'COMBINED_AST_B_TO_SOLVED': n.sum,
              'LOC_B_TO_SOLVED': n.sum,
              'IS_CONFLICT': any,
              'LOC_SIZE_SOLVED': n.sum,
              'NO_AUTHORS': n.sum,
-             'LOC_DIFF_BEFORE': n.sum,
-             'AST_DIFF_BEFORE': n.sum,
+             'LOC_DIFF': n.sum,
+             'AST_DIFF': n.sum,
              'MERGED_IN_MASTER': any,
              'PROJECT': first,
              'SHA_A': first,
@@ -29,7 +36,8 @@ groupDict = {'AST_A_BEFORE_SIZE': n.sum,
              'TIME_B': first,
              'NO_STATEMENTS': n.sum,
              'NO_METHODS': n.sum,
-             'NO_CLASSES': n.sum
+             'NO_CLASSES': n.sum,
+             'AFFECTED_NODES': groupNodes
 }
 
 def readData(folder):
@@ -46,12 +54,14 @@ def readData(folder):
             continue # Because some of the files are shorter
     return p.concat(dataFrames)
 
-data = readData("../../ase16/data-cost-conc-devel-ase16/merge-data")
+#data = readData("../../ase16/data-cost-conc-devel-ase16/merge-data")
+data = readData("../../data/with-types")
+data = data.fillna('')
 data['TIME_A'] = p.to_datetime(data['TIME_A'], unit='s')
 data['TIME_B'] = p.to_datetime(data['TIME_B'], unit='s')
 data['TIME_SOLVED'] = p.to_datetime(data['TIME_SOLVED'], unit='s')
-data['IS_CONFLICT'] = data['IS_CONFLICT'].replace({'TRUE': True, 'FALSE': False})
-data['MERGED_IN_MASTER'] = data['MERGED_IN_MASTER'].replace({'TRUE': True, 'FALSE': False})
+#data['IS_CONFLICT'] = data['IS_CONFLICT'].replace({'TRUE': True, 'FALSE': False})
+#data['MERGED_IN_MASTER'] = data['MERGED_IN_MASTER'].replace({'TRUE': True, 'FALSE': False})
 
 perCommit = data.groupby('SHA').aggregate(groupDict)
 print(perCommit.columns)
