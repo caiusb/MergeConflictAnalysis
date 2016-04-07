b1fc65cb0eb5068d871292716126f39e35d666f0
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2016-04-06 22:58:38 -0700

    Cleaning and processing and clustering
    At least an attempt for the later.

--- stats/cluster-commits.py
+++ stats/cluster-commits.py
@@ -1,27 +1,47 @@
-from sklearn import cluster, datasets, mixture
+from sklearn import cluster, datasets, mixture, preprocessing
+from sklearn.feature_extraction.text import CountVectorizer
 import matplotlib.pyplot as plt
 import csv
 import numpy as np
+import pandas as p
 
 def loadData():
-    data = []
-    with open("../data-clean-normal.csv") as f:
-        reader = csv.DictReader(f)
-        for line in reader:
-            observation = [line['LOC_A_TO_B'],
-                            line['LOC_A_TO_SOLVED'],
-                            line['LOC_B_TO_SOLVED'],
-                            line['NO_METHODS'],
-                            line['NO_CLASSES'],
-                            line['NO_STATEMENTS']
-                            ]
-            data.append(observation)
-
-
-    return data #[[float(a) for a in x] for x in data]
+    with open("../per-commit.csv") as f:
+        data = p.read_csv(f)
+    return data
 
 data = loadData()
+data['AFFECTED_NODES'] = data['AFFECTED_NODES'].fillna('')
 
-gmm = mixture.GMM()
-m = gmm.fit(data)
-print(m)
+data = data.loc[(data['IS_CONFLICT'] == True) &
+            (data['LOC_DIFF']) > 0 &
+            (data['LOC_A_TO_B'] > 0) &
+            (data['LOC_A_TO_SOLVED'] > 0) &
+            (data['LOC_B_TO_SOLVED'] > 0) &
+            (data['NO_METHODS'] > 0) &
+            (data['NO_STATEMENTS'] > 0) &
+            (data['NO_CLASSES'] > 0)]
+
+print(str(len(data)) + " observations")
+del data['IS_CONFLICT']
+
+toCluster = data[['LOC_DIFF', 'LOC_A_TO_B', 'LOC_A_TO_SOLVED', 'NO_METHODS', 'NO_STATEMENTS', 'NO_CLASSES', 'AFFECTED_NODES']]
+
+vectorizer = CountVectorizer(binary=True, lowercase=False)
+X = vectorizer.fit_transform(toCluster.AFFECTED_NODES)
+print(len(vectorizer.get_feature_names()))
+print(X.shape)
+del toCluster['AFFECTED_NODES']
+
+scaler = preprocessing.MinMaxScaler()
+toCluster = p.DataFrame(scaler.fit_transform(toCluster), columns=toCluster.columns.values)
+print(toCluster.shape)
+toCluster = p.concat([toCluster, p.DataFrame(X.toarray(), columns=vectorizer.get_feature_names())], axis=1)
+print(toCluster.shape)
+
+toCluster.to_csv("../data-ready.csv")
+
+birch = cluster.Birch()
+birch.fit(toCluster)
+print("==== Results ====")
+print("# of clusters: " + str(len(set(birch.labels_))))
