27efb39eb9a8b50015234ae87eb57f8a2ed391bd
Caius Brindescu <brindesc@oregonstate.edu>
2016-06-20 20:33:27 -0700

    This works
    Pandas DataFrame are terrible on a good day. This wasn't
    one of those.

--- stats/build-data.py
+++ stats/build-data.py
@@ -13,6 +13,7 @@ def readBuildData(folder):
             frame = p.read_csv(os.path.join(folder, f), skip_blank_lines=True)
             frame.columns = ['SHA', 'Parent1', 'Parent2', 'Merge']
             frame['Project'] = os.path.splitext(f)[0]
+            frame.set_index('SHA')
             frame.reset_index(inplace=True)
             frames.append(frame)
         except ValueError:
@@ -20,9 +21,33 @@ def readBuildData(folder):
 
     return p.concat(frames).drop('index', 1)
 
+def readSolvedStatus(folder):
+    files = [f for f in os.listdir(folder) if f.endswith('.csv')]
+    frames = []
+    for f in files:
+        try:
+            frame = p.read_csv(os.path.join(folder, f), skip_blank_lines=True)
+            frame.columns = ['SHA', 'Solved']
+            frame.set_index('SHA')
+            frame.reset_index(inplace=True)
+            frames.append(frame)
+        except ValueError:
+            continue
 
-folder = "../../build-data/results"
-buildData = readBuildData(folder)
-buildData.to_csv("../../data/build-data.csv", index=False)
+    return p.concat(frames).drop('index', 1)
+
+buildData = readBuildData("../../data/build-data/results")
+solved = readSolvedStatus("../../data/build-merge-data/results")
+
 print(buildData.shape)
 print(buildData.columns.values)
+print(solved.shape)
+print(solved.columns.values)
+
+buildData = p.merge(buildData, solved, on='SHA', how='outer')
+
+
+print(buildData.shape)
+print(buildData.columns.values)
+
+buildData.to_csv("../../data/build-data.csv")
