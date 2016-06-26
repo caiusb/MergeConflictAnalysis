7b73e9e16248f2d336477a4be72eb91037198de7
Caius Brindescu <brindesc@oregonstate.edu>
2016-06-25 21:21:11 -0700

    Simplified the interface of the Builder

--- MergeConflictAnalysis/src/main/scala/edu/oregonstate/mergeproblem/mergeconflictanalysis/Builder.scala
+++ MergeConflictAnalysis/src/main/scala/edu/oregonstate/mergeproblem/mergeconflictanalysis/Builder.scala
@@ -1,6 +1,6 @@
 package edu.oregonstate.mergeproblem.mergeconflictanalysis
 
-import java.io.{ByteArrayOutputStream, File, PrintStream}
+import java.io.File
 
 import org.apache.maven.shared.invoker.{DefaultInvocationRequest, DefaultInvoker, InvocationOutputHandler, InvocationResult}
 
@@ -12,44 +12,36 @@ object Builder {
     override def consumeLine(line: String) = {}
   }
 
-  def runMaven(projectPath: String, tasks: Seq[String]): Boolean =
-  runMaven(projectPath, tasks, new PrintStream(new ByteArrayOutputStream()))
-
-  def runMaven(projectPath: String, tasks: Seq[String], out: PrintStream): Boolean = {
-    //makeMavenHappy(projectPath)
-
-    val result = invoke(projectPath, tasks)
-
-    if (result.getExitCode != 0)
+  def runMaven(projectPath: String, tasks: Seq[String],
+               outputHandler: InvocationOutputHandler = nullOutputHandler,
+               errorHandler: InvocationOutputHandler = nullOutputHandler): Boolean =
+    if (invoke(projectPath, tasks, outputHandler, errorHandler).getExitCode != 0)
       return false
-    return true
-  }
+    else
+      return true
 
-  private def invoke(projectPath: String, tasks: Seq[String]): InvocationResult = {
+  private def invoke(projectPath: String, tasks: Seq[String],
+                     outputHandler: InvocationOutputHandler,
+                     errorHandler: InvocationOutputHandler): InvocationResult = {
     val request = new DefaultInvocationRequest
     request.setPomFile(new File(projectPath + "/pom.xml"))
     request.setGoals(seqAsJavaList(tasks))
     request.setInteractive(false)
     val invoker = new DefaultInvoker
-    invoker.setErrorHandler(nullOutputHandler)
-    invoker.setOutputHandler(nullOutputHandler)
+    invoker.setOutputHandler(outputHandler)
+    invoker.setErrorHandler(errorHandler)
     val result = invoker.execute(request)
     result
   }
 
-  def build(projectPath: String): Boolean = {
-    return runMaven(projectPath, Seq("clean", "compile"))
-  }
+  def build(projectPath: String): Boolean =
+    runMaven(projectPath, Seq("clean", "compile"))
 
   def test(projectPath: String): Boolean =
     runMaven(projectPath, Seq("clean", "test"))
 
-  def test(projectPath: String, out: PrintStream): Boolean =
-    runMaven(projectPath, Seq("clean", "test"), out)
-
-  def clean(projectPath: String): Boolean = {
+  def clean(projectPath: String): Boolean =
     runMaven(projectPath, Seq("clean"))
-  }
 
   private def makeMavenHappy(projectPath: String) =
     System.setProperty("maven.multiModuleProjectDirectory", projectPath)
