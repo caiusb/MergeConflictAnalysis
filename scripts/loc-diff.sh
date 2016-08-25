8a9b1419936e5ad12532d053aa04d474811d5762
Caius Brindescu <brindesc@oregonstate.edu>
2016-08-25 15:10:03 -0700

    Correctly calculating the diff

---
+++ scripts/loc-diff.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+for i in *
+do
+    project=$i
+    cd $i
+    output=`git log --merges --format="%H"`
+    printf "%s\n" $output | while read sha
+    do
+        parents=`git log --format="%P" $sha`
+        result=`git diff --stat $parents | grep -E "java\s" | tr -s ' ' | cut -d" " -f4 | paste -sd+ - | bc`
+        if [ -z $result ]
+        then
+            result="0"
+        fi
+        echo $project,$sha,$result
+    done
+    cd ../
+done
