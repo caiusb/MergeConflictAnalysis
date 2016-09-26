954134c9d0881722008ff02e7b50993f144e9158
Caius Brindescu <brindesc@oregonstate.edu>
2016-09-25 21:58:23 -0700

    Moar data

--- MergeConflictAnalysis/src/main/scala/edu/oregonstate/mergeproblem/mergeconflictanalysis/processors/ConflictContentProcessor.scala
+++ MergeConflictAnalysis/src/main/scala/edu/oregonstate/mergeproblem/mergeconflictanalysis/processors/ConflictContentProcessor.scala
@@ -12,19 +12,19 @@ import scala.collection.mutable
 
 object ConflictContentProcessor {
 
-	def getHeader: String = "SHA,FILE_NAME,A_EMAIL,B_EMAIL,S_EMAIL,A_CONFLICT,B_CONFLICT,S_CONFLICT,S_DIFF_A,S_DIFF_B,A_TOTAL,B_TOTAL"
+	def getHeader: String = "SHA,FILE_NAME,A_EMAIL,B_EMAIL,S_EMAIL," +
+		"A_CONFLICT,B_CONFLICT,S_NEW," +
+		"S_NEW_A,S_NEW_B," +
+		"A_S,B_S" +
+		"A_TOTAL,B_TOTAL"
 
 	def getData(status: CommitStatus, fileName: String): String = {
 		val cf = status.getCombinedFile(fileName)
 		val a = cf.getVersion(A)
 		val b = cf.getVersion(B)
-		val cl = cf.getConflictingLines
 		val s = status.getSolvedVersion(fileName)
 
 		val diffAlgo = DiffAlgorithm.getAlgorithm(DiffAlgorithm.SupportedAlgorithm.MYERS)
-
-		val conflictingLines = cf.getConflictingLines.map(l => (l, cf.getChunksForLine(l))).toMap
-		val solved = Util.retrieveFile(status.getRepository, status.getSHA1, fileName)
 
 		val aLines = cf.getChunksForOwner(ChunkOwner.A).flatMap(c => Range(c.getBeginLine, c.getEndLine)).distinct
 		val bLines = cf.getChunksForOwner(ChunkOwner.B).flatMap(c => Range(c.getBeginLine, c.getEndLine)).distinct
@@ -32,12 +32,12 @@ object ConflictContentProcessor {
 		val diffAS = diffAlgo.diff(RawTextComparator.DEFAULT, new RawText(a.getBytes), new RawText(s.getBytes))
 		val diffBS = diffAlgo.diff(RawTextComparator.DEFAULT, new RawText(b.getBytes), new RawText(s.getBytes))
 
-		val newSLinesToA = diffAS.flatMap(e => Range(e.getBeginB, e.getEndB)).distinct.diff(aLines)
-		val newSLinesToB = diffBS.flatMap(e => Range(e.getBeginB, e.getEndB)).distinct.diff(bLines)
+		val newSLinesToA = diffAS.flatMap(e => Range(e.getBeginA, e.getEndA)).distinct.diff(aLines)
+		val newSLinesToB = diffBS.flatMap(e => Range(e.getBeginA, e.getEndA)).distinct.diff(bLines)
 		val newSLines = newSLinesToA.union(newSLinesToB)
 
-		val aContribution = cf.getChunksForOwner(ChunkOwner.A).map(_.getContent).mkString.split("\n")
-		val bContribution = cf.getChunksForOwner(ChunkOwner.B).map(_.getContent).mkString.split("\n")
+		val linesInAandS = aLines.diff(newSLinesToA)
+		val linesInBandS = bLines.diff(newSLinesToB)
 
 		val repo = status.getRepository
 		val aAuthor = CommitUtils.getCommit(repo, status.getASHA).getCommitterIdent.getEmailAddress
@@ -49,7 +49,8 @@ object ConflictContentProcessor {
 
 		return status.getSHA1 + "," + fileName + "," + aAuthor + "," + bAuthor + "," + sAuthor +
 			"," + aLines.size + "," + bLines.size + "," + newSLines.size +
-			"," + newSLinesToA + "," + newSLinesToB +
+			"," + newSLinesToA.size + "," + newSLinesToB.size +
+		 	"," + linesInAandS.size + "," + linesInBandS.size +
 			"," + aMergedLine.size + "," + bMergedLine.size
 	}
 }
