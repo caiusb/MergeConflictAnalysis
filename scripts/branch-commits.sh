250d6fc2db8f22c5c2008280650401dfc15ee476
Caius Brindescu <brindesc@oregonstate.edu>
2016-09-28 18:00:27 -0700

    Using the common infra

--- scripts/branch-commits.sh
+++ scripts/branch-commits.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-repo=$1
-
-merges=`git log --merges --format="%H"`
+source 'common.sh'
 
 function printBranch() {
     sha=$1
@@ -14,21 +12,26 @@ function printBranch() {
     done
 }
 
-printf "%s" "$merges" | while read merge
-do
-    parents=`git show --format="%P" -s $merge`
-    firstParent=`echo $parents | cut -d" " -f1`
-    secondParent=`echo $parents | cut -d" " -f2`
-    firstDate=`git show --format="%at" -s $firstParent`
-    secondDate=`git show --format="%at" -s $secondParent`
-    if [ $firstDate -gt $secondDate ]
-    then
-        firstParent=$secondParent
-        secondParent=`echo $parents | cut -d" " -f1`
-    fi
-    base=`git merge-base $firstParent $secondParent`
-    aCommits=`git log --format="%H,%at" $base..$firstParent`
-    printBranch $merge "A" "$aCommits"
-    bCommits=`git log --format="%H,%at" $base..$secondParent`
-    printBranch $merge "B" "$bCommits"
-done
+function getBranchCommits() {
+    repo=$1
+    resultFolder=$2
+    merges=`git log --merges --format="%H"`
+    printf "%s" "$merges" | while read merge
+    do
+        parents=`git show --format="%P" -s $merge`
+        firstParent=`echo $parents | cut -d" " -f1`
+        secondParent=`echo $parents | cut -d" " -f2`
+        firstDate=`git show --format="%at" -s $firstParent`
+        secondDate=`git show --format="%at" -s $secondParent`
+        if [ $firstDate -gt $secondDate ]
+        then
+            firstParent=$secondParent
+            secondParent=`echo $parents | cut -d" " -f1`
+        fi
+        base=`git merge-base $firstParent $secondParent`
+        aCommits=`git log --format="%H,%at" $base..$firstParent`
+        printBranch $merge "A" "$aCommits" > $resultFolder/$repo.csv
+        bCommits=`git log --format="%H,%at" $base..$secondParent`
+        printBranch $merge "B" "$bCommits" > $resultFolder/$repo.csv
+    done
+}
