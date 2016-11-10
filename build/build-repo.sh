2b1e69db1249d7501dff57bf5a37bb646cb01ac9
Caius Brindescu <brindesc@oregonstate.edu>
2016-11-10 14:31:39 -0800

    Making the thing work

--- build/build-repo.sh
+++ build/build-repo.sh
@@ -2,9 +2,11 @@
 
 cd $1
 
+git checkout -f master > /dev/null 2>&1
+
 if [ -e "pom.xml" ]
 then
-	buildCmd="maven build"
+	buildCmd="mvn build"
 	testCmd="mvn test"
 elif [ -e "build.gradle" ]
 then
@@ -14,24 +16,23 @@ else
 	echo "Unknown build system"
 fi
 
-
-git log --merges | while read sha
+git log --merges --format="%H" | while read sha
 do
-	parents=`git show $sha --format="%P"`
+	parents=`git show $sha --format="%P" --quiet`
 	p1=`echo $parents | cut -d" " -f1`
 	p2=`echo $parents | cut -d" " -f2`
-	git checkout -f $p1 > /dev/nul
-	git merge $p2 > /dev/null
+	git checkout -f $p1 > /dev/null 2>&1
+	git merge $p2 > /dev/null 2>&1
 	if [ $? -ne 0 ]
 	then
  		status="conflict"
  	else
- 		$buildCmd > /dev/null
- 		if [ $? -ne 0]
+ 		$buildCmd > /dev/null 2>&1
+ 		if [ $? -ne 0 ]
  		then
  			status="build"
  		else
- 			$testCmd > /dev/null
+ 			$testCmd > /dev/null 2>&1
  			if [ $? -ne 0 ]
  			then
  				status="test"
